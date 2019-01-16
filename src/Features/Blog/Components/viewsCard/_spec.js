2e6735198b4fd569af405890f94232fcb1ea4d75
Giancarlo Galliani <galliani.08@gmail.com>
2019-01-16 20:27:10 +0000

    Add ViewsCard, ViewsContainer, and Title Components

--- src/Features/Blog/Components/viewsCard/_spec.js
+++ src/Features/Blog/Components/viewsCard/_spec.js
@@ -1,3 +1,10 @@
-xtest('test placheolder', () => {
-  
+import React from 'react'
+import { shallow } from 'enzyme'
+import ViewsCard from './'
+
+describe('ViewsCard', () => {
+  it('should render with default values', () => {
+    const wrapper = shallow(<ViewsCard />)
+    expect(wrapper).toMatchSnapshot()
+  });
 });
--- src/Features/Blog/Containers/viewsContainer/_spec.js
+++ src/Features/Blog/Containers/viewsContainer/_spec.js
@@ -1,3 +1,10 @@
-xtest('test placeholder', () => {
-  
+import React from 'react'
+import { shallow } from 'enzyme'
+import ViewsContainer from './'
+
+describe('ViewsContainer', () => {
+  it('should render with default values', () => {
+    const wrapper = shallow(<ViewsContainer />)
+    expect(wrapper).toMatchSnapshot()
+  });
 });
