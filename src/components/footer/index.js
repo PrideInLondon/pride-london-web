af873e5f222e0a031d59b52ea56c8278c51db2e3
Jason <jase_lai@hotmail.com>
2019-03-01 12:39:21 +0000

    add back missing input component for footer and icons, make nav and footer full screen width

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { darken, rgba } from 'polished'
 import { media } from '../../theme/media'
+import theme from '../../theme/theme'
 import NewsletterForm from '../../features/events/components/newsletter'
 import { Row, Column } from '../grid'
 import Facebook from '!svg-react-loader?name=Facebook!../../theme/assets/images/icon-facebook.svg' // eslint-disable-line
@@ -11,8 +12,12 @@ import Youtube from '!svg-react-loader?name=Youtube!../../theme/assets/images/ic
 import LinkedIn from '!svg-react-loader?name=LinkedIn!../../theme/assets/images/icon-linkedin.svg' // eslint-disable-line
 import Snapchat from '!svg-react-loader?name=Snapchat!../../theme/assets/images/icon-snapchat.svg' // eslint-disable-line
 
+const FooterWrapper = styled.div`
+  background-color: ${theme.colors.indigo};
+`
+
 const StyledFooter = styled.footer`
-  background-color: ${props => props.theme.colors.indigo};
+  background-color: ${theme.colors.indigo};
   max-width: 1440px;
   width: 100%;
   margin: 0 auto;
@@ -24,7 +29,7 @@ const Heading = styled.h2`
   line-height: 1.375rem;
   font-weight: 600;
   margin: 0 0 1.25rem 0;
-  color: ${props => props.theme.colors.white};
+  color: ${theme.colors.white};
 
   ${media.tablet`
     font-size: 1.5rem;
@@ -58,17 +63,17 @@ const SocialLink = styled.a`
     svg {
       path {
         transition: fill 0.15s linear;
-        fill: ${props => darken(0.1, props.theme.colors.eucalyptusGreen)};
+        fill: ${darken(0.1, theme.colors.eucalyptusGreen)};
       }
     }
   }
 `
 const Small = styled.small`
   padding-top: 30px;
-  border-top: 1px solid ${props => rgba(props.theme.colors.white, 0.3)};
+  border-top: 1px solid ${rgba(theme.colors.white, 0.3)};
   display: block;
   font-size: 0.75rem;
-  color: ${props => props.theme.colors.white};
+  color: ${theme.colors.white};
 
   ${media.tablet`
     margin-top: 40px;
@@ -76,73 +81,75 @@ const Small = styled.small`
 `
 
 export const Footer = () => (
-  <StyledFooter>
-    <Row>
-      <Column width={[1, 1, 1, 2 / 3]}>
-        <NewsletterForm />
-      </Column>
-      <Column width={[1, 1, 1, 1 / 3]}>
-        <Heading>Follow us on</Heading>
-        <SocialList>
-          <SocialItem>
-            <SocialLink
-              href="https://www.facebook.com/pg/LondonLGBTPride"
-              aria-label="Link to Facebook page"
-            >
-              <Facebook />
-            </SocialLink>
-          </SocialItem>
-          <SocialItem>
-            <SocialLink
-              href="https://twitter.com/PrideInLondon"
-              aria-label="Link to Twitter page"
-            >
-              <Twitter />
-            </SocialLink>
-          </SocialItem>
-          <SocialItem>
-            <SocialLink
-              href="http://instagram.com/prideinlondon"
-              aria-label="Link to Instagram page"
-            >
-              <Instagram />
-            </SocialLink>
-          </SocialItem>
-          <SocialItem>
-            <SocialLink
-              href="https://www.youtube.com/user/LondonLGBTPride?sub_confirmation=1"
-              aria-label="Link to Youtube channel"
-            >
-              <Youtube />
-            </SocialLink>
-          </SocialItem>
-          <SocialItem>
-            <SocialLink
-              href="https://www.linkedin.com/company/prideinlondon"
-              aria-label="Link to LinkedIn page"
-            >
-              <LinkedIn />
-            </SocialLink>
-          </SocialItem>
-          <SocialItem>
-            <SocialLink
-              href="http://www.snapchat.com/add/LondonLGBTPride"
-              aria-label="Link to Snapchat page"
-            >
-              <Snapchat />
-            </SocialLink>
-          </SocialItem>
-        </SocialList>
-      </Column>
-      <Column width={1}>
-        <Small>
-          London LGBT+ Community Pride CIC, PO Box 71920, London NW2 9QN -
-          Registered in England and Wales as a Community Interest Company (no.
-          8321669)
-        </Small>
-      </Column>
-    </Row>
-  </StyledFooter>
+  <FooterWrapper>
+    <StyledFooter>
+      <Row>
+        <Column width={[1, 1, 1, 2 / 3]}>
+          <NewsletterForm />
+        </Column>
+        <Column width={[1, 1, 1, 1 / 3]}>
+          <Heading>Follow us on</Heading>
+          <SocialList>
+            <SocialItem>
+              <SocialLink
+                href="https://www.facebook.com/pg/LondonLGBTPride"
+                aria-label="Link to Facebook page"
+              >
+                <Facebook />
+              </SocialLink>
+            </SocialItem>
+            <SocialItem>
+              <SocialLink
+                href="https://twitter.com/PrideInLondon"
+                aria-label="Link to Twitter page"
+              >
+                <Twitter />
+              </SocialLink>
+            </SocialItem>
+            <SocialItem>
+              <SocialLink
+                href="http://instagram.com/prideinlondon"
+                aria-label="Link to Instagram page"
+              >
+                <Instagram />
+              </SocialLink>
+            </SocialItem>
+            <SocialItem>
+              <SocialLink
+                href="https://www.youtube.com/user/LondonLGBTPride?sub_confirmation=1"
+                aria-label="Link to Youtube channel"
+              >
+                <Youtube />
+              </SocialLink>
+            </SocialItem>
+            <SocialItem>
+              <SocialLink
+                href="https://www.linkedin.com/company/prideinlondon"
+                aria-label="Link to LinkedIn page"
+              >
+                <LinkedIn />
+              </SocialLink>
+            </SocialItem>
+            <SocialItem>
+              <SocialLink
+                href="http://www.snapchat.com/add/LondonLGBTPride"
+                aria-label="Link to Snapchat page"
+              >
+                <Snapchat />
+              </SocialLink>
+            </SocialItem>
+          </SocialList>
+        </Column>
+        <Column width={1}>
+          <Small>
+            London LGBT+ Community Pride CIC, PO Box 71920, London NW2 9QN -
+            Registered in England and Wales as a Community Interest Company (no.
+            8321669)
+          </Small>
+        </Column>
+      </Row>
+    </StyledFooter>
+  </FooterWrapper>
 )
 
 export default Footer
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,6 +6,7 @@ import { ThemeProvider } from 'styled-components'
 import { Provider } from '../components/appContext'
 // import Navigation from '../components/navigation'
 import SimpleNav from '../components/simpleNav'
+import Footer from '../components/footer'
 import theme from '../theme/theme'
 import favicon from '../favicon.ico'
 
@@ -72,6 +73,7 @@ const Layout = props => (
               />
               <SimpleNav />
               <main>{props.children}</main>
+              <Footer />
             </Fragment>
           </ThemeProvider>
         </Provider>
