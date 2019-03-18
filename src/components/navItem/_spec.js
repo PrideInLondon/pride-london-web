d6e58c261cfb2770f976a3526033f88bb36d2966
Jason <jase_lai@hotmail.com>
2019-03-18 17:20:10 +0000

    add mobile tests for navItem

--- src/components/navItem/_spec.js
+++ src/components/navItem/_spec.js
@@ -62,6 +62,12 @@ describe('Desktop <NavItem/>', () => {
         expect(toJSON(wrapper)).toMatchSnapshot()
     })
 
+
+    it('Should have the aria-haspopup attribute', ()=> {
+        const wrapper = shallow(<NavItem item={mockData} />)
+        expect(wrapper.find('navItem__SubmenuToggle').prop('aria-haspopup')).toBeTruthy()
+    })
+
     it('Should display the submenu on mouseenter and hide it on mouseleave', () => {
         const wrapper = mount(<NavItem item={mockData} />)
         wrapper.simulate('mouseEnter')
@@ -71,6 +77,65 @@ describe('Desktop <NavItem/>', () => {
         wrapper.simulate('mouseLeave')
         wrapper.update();
         expect(wrapper.find('navItem__SubmenuToggle').prop('aria-expanded')).toBeFalsy()
+    })
+
+    it('Should toggle the submenu on click', () => {
+        const wrapper = mount(<NavItem item={mockData} />)
+        const SubmenuToggle = wrapper.find('navItem__SubmenuToggle')
+        const isOpen = SubmenuToggle.prop('aria-expanded')
+
+        SubmenuToggle.simulate('click')
+        wrapper.update();
+        expect(wrapper.find('navItem__SubmenuToggle').prop('aria-expanded')).toBe(!isOpen)
+
+        SubmenuToggle.simulate('click')
+        wrapper.update();
+        expect(wrapper.find('navItem__SubmenuToggle').prop('aria-expanded')).toBe(isOpen)
+    })
+
+})
+
+
+// Tests for Mobile version of Nav
+describe('Mobile <NavItem/>', () => {
+    beforeAll(() => {
+        global.___loader = {
+          enqueue: jest.fn(),
+        }
+
+        global.window.matchMedia = jest.fn(() => { 
+            return { 
+                matches: false, 
+                addListener: jest.fn(), 
+                removeListener: jest.fn() 
+            } 
+        })
+    })
+    afterAll(() => {
+        global.___loader.enqueue.mockReset()
+        global.window.matchMedia.mockReset()
+    })
+
+    it('renders and matches snapshot', () => {
+        const wrapper = shallow(<NavItem item={mockData} />)
+        expect(toJSON(wrapper)).toMatchSnapshot()
+    })
+
+    it('Should not have the aria-haspopup attribute', ()=> {
+        const wrapper = shallow(<NavItem item={mockData} />)
+        expect(wrapper.find('navItem__SubmenuToggle').prop('aria-haspopup')).toBeFalsy()
+    })
+
+    it('Should not open/close on mouseenter or mouseleave', () => {
+        const wrapper = mount(<NavItem item={mockData} />)
+        const isOpen = wrapper.find('navItem__SubmenuToggle').prop('aria-expanded')
+        wrapper.simulate('mouseEnter')
+        wrapper.update();
+        expect(wrapper.find('navItem__SubmenuToggle').prop('aria-expanded')).toBe(isOpen)
+
+        wrapper.simulate('mouseLeave')
+        wrapper.update();
+        expect(wrapper.find('navItem__SubmenuToggle').prop('aria-expanded')).toBe(isOpen)
     })
 
     it('Should toggle the submenu on click', () => {
