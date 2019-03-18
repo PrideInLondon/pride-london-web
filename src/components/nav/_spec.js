5fe15de3aa25e743156111658a45a5967a68ad04
Jason <jase_lai@hotmail.com>
2019-03-18 12:43:17 +0000

    add __PATH_PREFIX__ to jest config to prevent gatsby Link component error

--- src/components/nav/_spec.js
+++ src/components/nav/_spec.js
@@ -1,19 +1,48 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-// import Nav from '../nav'
-// import NavItem from '../navItem'
-// import Submenu from '../submenu'
+import { shallow, mount } from 'enzyme'
+import Nav from '../nav'
+import NavItem from '../navItem'
+import Submenu from '../submenu'
 import 'jest-styled-components'
+import toJSON from 'enzyme-to-json'
 
 // // SVGs need to be mocked as the SVG output causes Babel to throw
 // jest.mock('../../theme/assets/images/logo-pride.svg', () => 'logo')
 // jest.mock('../../theme/assets/images/logo-pride.svg', () => 'logo')
 // jest.mock('../../theme/assets/images/logo-pride.svg', () => 'logo')
 
+// window.matchMedia = window.matchMedia || function() {
+//     return {
+//         matches : false,
+//         addListener : function() {},
+//         removeListener: function() {}
+//     };
+// };
+
 describe('<Nav/>', () => {
+    beforeAll(() => {
+        global.___loader = {
+          enqueue: jest.fn(),
+        }
+
+        global.window.matchMedia = jest.fn(() => { 
+            return { 
+                matches: false, 
+                addListener: jest.fn(), 
+                removeListener: jest.fn() 
+            } 
+        })
+    })
+    afterAll(() => {
+        global.___loader.enqueue.mockReset()
+        global.window.matchMedia.mockReset()
+    })
+
     it('renders and matches snapshot', () => {
-        const wrapper = shallow(<Nav />)
+        const wrapper = mount(<Nav />)
         console.log(wrapper.debug())
-        expect(wrapper).toMatchSnapshot()
-      })
+        // console.log("BUTTON",wrapper.find('button').debug())
+
+        expect(toJSON(wrapper)).toMatchSnapshot()
+    })
 })
