3d5c7c7ccb45c2cb42a47213d2d9a93b24e6d4e1
smoisset <smoisset@worldremit.com>
2019-03-20 22:11:58 +0000

    Format tests

--- src/features/events/helpers/_spec.js
+++ src/features/events/helpers/_spec.js
@@ -1,29 +1,29 @@
 import { filterPastEvents } from '../helpers'
 
 const yesterday = new Date()
-yesterday.setDate(yesterday.getDate() - 1);
+yesterday.setDate(yesterday.getDate() - 1)
 
 const tomorrow = new Date()
-tomorrow.setDate(tomorrow.getDate() + 1);
+tomorrow.setDate(tomorrow.getDate() + 1)
 
 const futureEvent = {
-    node: {
-        startTime: tomorrow
-    }
+  node: {
+    startTime: tomorrow,
+  },
 }
 
 const pastEvent = {
-    node: {
-        startTime: yesterday
-    }
+  node: {
+    startTime: yesterday,
+  },
 }
 
 describe('filterPastEvents', () => {
-    it('Should return true if date is after today', () => {
-        expect(filterPastEvents(futureEvent)).toBeTruthy()
-    })
+  it('returns true if date is after today', () => {
+    expect(filterPastEvents(futureEvent)).toBeTruthy()
+  })
 
-    it('Should return false if date is in the past', () => {        
-        expect(filterPastEvents(pastEvent)).toBeFalsy()
-    })
+  it('returns false if date is in the past', () => {
+    expect(filterPastEvents(pastEvent)).toBeFalsy()
+  })
 })
