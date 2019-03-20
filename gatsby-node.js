c8b0715ba5d9950876984aa9fb85d82f74db909e
laij84 <jase_lai@hotmail.com>
2019-03-20 22:23:22 +0000

    Fix/event detail (#99)
    * fix generating event detail pages for recurrence dates
    
    * update snapshots
    
    * remove unnecessary parseZone
    
    * add test for filterPastEvents function
    
    * Format tests

--- gatsby-node.js
+++ gatsby-node.js
@@ -57,30 +57,35 @@ exports.createPages = async ({ graphql, actions }) => {
     const eventTemplate = path.resolve('./src/templates/event.js')
     const prettyDate = 'D MMM YYYY'
     // Don't create pages for past events
-    result.data.events.edges.filter(filterPastEvents).forEach(edge => {
+    result.data.events.edges.forEach(edge => {
       if (!edge.node.recurrenceDates) {
-        createPage({
-          // Each page is required to have a `path` as well
-          // as a template component. The `context` is
-          // optional but is often necessary so the template
-          // can query data specific to each page.
-          path: `/events/${edge.node.id}/`,
-          component: eventTemplate,
-          context: {
-            id: edge.node.id,
-            startDate: moment(edge.node.startTime).format(prettyDate),
-            endDate: moment(edge.node.endTime).format(prettyDate),
-            startTime: formatTime(edge.node.startTime),
-            endTime: formatTime(edge.node.endTime),
-          },
-        })
+        //   Filter past events
+        if (filterPastEvents(edge)) {
+          createPage({
+            // Each page is required to have a `path` as well
+            // as a template component. The `context` is
+            // optional but is often necessary so the template
+            // can query data specific to each page.
+            path: `/events/${edge.node.id}/`,
+            component: eventTemplate,
+            context: {
+              id: edge.node.id,
+              startDate: moment
+                .parseZone(edge.node.startTime)
+                .format(prettyDate),
+              endDate: moment.parseZone(edge.node.endTime).format(prettyDate),
+              startTime: formatTime(edge.node.startTime),
+              endTime: formatTime(edge.node.endTime),
+            },
+          })
+        }
       } else {
         const recurrenceDates = sanitizeDates([
-          moment(edge.node.startTime).format(dateFormat),
+          moment.parseZone(edge.node.startTime).format(dateFormat),
           ...edge.node.recurrenceDates,
         ])
 
-        recurrenceDates.forEach(date => {
+        recurrenceDates.filter(filterPastEvents).forEach(date => {
           const customId = `${edge.node.id}-${date.split('/').join('')}`
           createPage({
             path: `/events/${customId}/`,
