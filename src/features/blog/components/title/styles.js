bdbd0470d458416c9a0f42979a05ba063717917f
Giancarlo Galliani <galliani.08@gmail.com>
2019-03-22 10:44:14 +0000

    Fix theme color node

--- src/features/blog/components/title/styles.js
+++ src/features/blog/components/title/styles.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
+import theme from '../../../../theme/theme'
 
 export const StyledTitle = styled.h2`
   font-weight: 700;
   color: ${props =>
-    props.isLight ? props.theme.colors.lightGrey : props.theme.colors.darkBlue};
+    props.isLight ? theme.colors.lightGrey : theme.colors.darkBlue};
   margin: 0.25em;
   text-align: ${props => (props.isCentered ? 'center' : 'inherit')};
 `
