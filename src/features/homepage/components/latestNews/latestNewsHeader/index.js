5656583cc6d422a5bcf1e074cad5bd07d10d00c5
rgryb <49141558+rgryb@users.noreply.github.com>
2019-04-03 12:56:21 +0200

    Latest News section (#179)
    * latestNews
    
    * default-tests
    
    * remove static query dep test
    
    * move to homepage
    
    * empty line clean
    
    * update test snap

---
+++ src/features/homepage/components/latestNews/latestNewsHeader/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Header, Link, Title } from './styles'
+
+const LatestNewsHeader = () => (
+  <Header>
+    <Title>Latest News</Title>
+    <Link>View all news ›</Link>
+  </Header>
+)
+
+export default LatestNewsHeader
