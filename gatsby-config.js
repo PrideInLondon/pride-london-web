1ada7f70f2c95fbf17df6d2758bbe07c737444d5
Jason <jase_lai@hotmail.com>
2019-04-04 18:01:46 +0100

    add gatsby-plugin-robots to stop crawlers on netlify staging

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,18 @@
 const env = process.env.NODE_ENV || 'development'
 require('dotenv').config({ path: `.env.${env}` })
 
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://prideinlondon.org/',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+const isNetlifyProduction = NETLIFY_ENV === 'production'
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
+
 module.exports = {
   siteMetadata: {
+    siteUrl,
     title: 'Pride in London Event Listing',
   },
   plugins: [
@@ -24,6 +34,27 @@ module.exports = {
         captureUnhandledRejections: true,
         payload: {
           environment: 'development',
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }],
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
         },
       },
     },
