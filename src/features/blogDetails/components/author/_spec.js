2d0ba86784ba1a6d3fa37917cdba50716f8cd0a1
Robert Grybowicz <robert.grybowicz@pagepro.pl>
2019-04-08 09:54:52 +0200

    mock data for Author

--- src/features/blogDetails/components/author/_spec.js
+++ src/features/blogDetails/components/author/_spec.js
@@ -4,7 +4,16 @@ import Author from '.'
 
 describe(Author.name, () => {
   it('renders with default values', () => {
-    const wrapper = shallow(<Author />)
+    const wrapper = shallow(
+      <Author
+        author={{
+          imageSrc: 'imageSrc',
+          display_name: {
+            display_name: 'FullName',
+          },
+        }}
+      />
+    )
     expect(wrapper).toMatchSnapshot()
   })
 })
