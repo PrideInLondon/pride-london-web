393d9f1af1c1f81bc7bc1e6b8099c2b9c5e0aff2
CodeDev Przemo <35106250+codedev-exp@users.noreply.github.com>
2019-04-09 11:00:28 +0200

    Announcements (#204)
    * Announcements sections
    
    * fix test
    
    * fix test
    
    * fix padding announcements
    
    * Update snapshot

---
+++ src/features/homepage/components/announcementHeader/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+import { media } from '../../../../theme/media'
+
+export const Title = styled.p`
+  font-size: 18px;
+  line-height: 1.44;
+`
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: left;
+  flex-grow: 1;
+  flex-direction: column;
+  margin: 30px 0 0;
+  padding-left: 5px;
+`
+export const Head = styled.h2`
+  margin: 0;
+  font-weight: bold;
+  font-size: 26px;
+
+  ${media.tablet`
+    font-size: 32px;
+  `};
+`
