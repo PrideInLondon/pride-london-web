ed3ec0f7c2edf35d8bdfda2a2387901eb4a89261
smoisset <smoisset@worldremit.com>
2019-04-11 13:46:06 +0100

    Extract styles from bannerImage

--- src/components/bannerImage/_spec.js
+++ src/components/bannerImage/_spec.js
@@ -6,47 +6,47 @@ import BannerSubtitle from '../banner/bannerSubtitle'
 import BannerImage from '.'
 import 'jest-styled-components'
 
-describe('BannerImage', () => {
-  it('renders', () => {
+describe('<BannerImage />', () => {
+  it('renders the component <BannerImage />', () => {
     const wrapper = shallow(<BannerImage />)
     expect(toJSON(wrapper)).toMatchSnapshot()
   })
 
-  it('renders a <BannerTitle />', () => {
+  it('renders the component <BannerTitle />', () => {
     const wrapper = shallow(<BannerImage />)
     expect(wrapper.find('StyledBannerTitle')).toHaveLength(1)
   })
 
-  it('renders a <BannerSubtitle />', () => {
+  it('renders the component <BannerSubtitle />', () => {
     const wrapper = shallow(<BannerImage />)
     expect(wrapper.find('StyledBannerSubtitle')).toHaveLength(1)
   })
 
-  it('renders a <BannerDate /> if passed a date prop', () => {
+  it('renders the component <BannerDate /> if  a date prop is passed', () => {
     const date = 'Saturday 6th July'
     const wrapper = shallow(<BannerImage date={date} />)
     expect(wrapper.find('StyledBannerDate')).toHaveLength(1)
   })
 
-  it('renders a background image on the wrapper if passed an imageSrc prop and imageFullWidth prop', () => {
+  it('renders a background image on the wrapper if an imageSrc prop and imageFullWidth prop are passed', () => {
     const src = 'foo'
     const wrapper = mount(<BannerImage imageSrc={src} imageFullWidth />)
-    expect(wrapper.find('bannerImage__StyledWrapper')).toHaveStyleRule(
+    expect(wrapper.find('styles__StyledWrapper')).toHaveStyleRule(
       'background-image',
       `url(${src})`
     )
   })
 
-  it('renders a background image on the container if passed an imageSrc prop', () => {
+  it('renders a background image on the container if an imageSrc prop is passed', () => {
     const src = 'foo'
     const wrapper = mount(<BannerImage imageSrc={src} />)
-    expect(wrapper.find('bannerImage__StyledContainer')).toHaveStyleRule(
+    expect(wrapper.find('styles__StyledContainer')).toHaveStyleRule(
       'background-image',
       `url(${src})`
     )
   })
 
-  it('renders the titleText from props to BannerTitle ', () => {
+  it('renders the titleText from props to the component BannerTitle', () => {
     const titleText = 'Here is a test title!'
     const wrapper = shallow(<BannerImage titleText={titleText} />)
     expect(toJSON(wrapper)).toMatchSnapshot()
@@ -58,7 +58,7 @@ describe('BannerImage', () => {
     ).toBe(titleText)
   })
 
-  it('renders the subtitleText from props to BannerSubtitle', () => {
+  it('renders the subtitleText from props to the component BannerSubtitle', () => {
     const subtitleText = 'And here is a test subtitle!'
     const wrapper = shallow(<BannerImage subtitleText={subtitleText} />)
     expect(toJSON(wrapper)).toMatchSnapshot()
---
+++ src/components/bannerImage/styles.js
@@ -0,0 +1,81 @@
+import styled, { css } from 'styled-components'
+import { media } from '../../theme/media'
+import { Row, Container } from '../grid'
+
+export const StyledContainer = styled(Container)`
+  flex-grow: 1;
+  align-items: center;
+  display: flex;
+  align-self: stretch;
+  padding-bottom: ${props => (props.homepage ? '17vh' : '0')};
+
+  ${media.tablet`
+    padding-bottom: ${props => (props.homepage ? '35vh' : '0')};
+  `};
+
+  ${props =>
+    props.imageSrc &&
+    !props.imageFullWidth &&
+    css`
+      background-image: url(${props.imageSrc});
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position: right bottom;
+    `}
+`
+
+export const StyledRow = styled(Row)`
+  width: 100%;
+
+  ${media.tabletMax`
+    align-self: flex-end;
+  `};
+`
+
+export const StyledWrapper = styled.div`
+  display: flex;
+  min-height: 270px;
+  overflow: hidden;
+  position: relative;
+  background-color: ${props => props.color};
+  height: ${props => props.large && '400px'};
+
+  
+    ${props =>
+      props.imageSrc &&
+      props.imageFullWidth &&
+      css`
+        background-image: url(${props.imageSrc});
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center center;
+      `}
+
+  ${props =>
+    props.allowContentUnderflow &&
+    css`
+      align-items: flex-start;
+      min-height: 380px;
+      padding-top: 50px;
+      margin-bottom: -75px;
+    `}
+
+
+  ${media.mobile`
+    height: ${props => props.large && '400px'};
+  `};
+
+  ${media.tablet`
+    align-items: center;
+    height: ${props => (props.large ? '500px' : '400px')};
+    padding: 0;
+  `};
+
+  ${media.desktop`
+    height: ${props => props.large && '650px'};
+  `};
+
+  ${media.desktopHD`
+    height: ${props => props.large && '800px'};
+  `};
+`
