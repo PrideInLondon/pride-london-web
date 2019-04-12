306b0504b252a9b9272d4c7bf1c78bd87dd5633b
Robert Grybowicz <robert.grybowicz@pagepro.pl>
2019-04-12 09:08:57 +0200

    update test

--- src/features/blog/containers/newsCards/_spec.js
+++ src/features/blog/containers/newsCards/_spec.js
@@ -4,7 +4,18 @@ import NewsCards from '.'
 
 describe(NewsCards.name, () => {
   it('renders with default values', () => {
-    const wrapper = shallow(<NewsCards articles={[]} />)
+    const wrapper = shallow(
+      <NewsCards
+        articles={[
+          {
+            category: {},
+            title: 'title',
+            id: 'id',
+            datePublished: '2019-03-28T20:30+00:00',
+          },
+        ]}
+      />
+    )
     expect(wrapper).toMatchSnapshot()
   })
 })
