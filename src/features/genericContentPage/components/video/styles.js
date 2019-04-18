9dadc5e3fe6f6bdebbeb20697b7c5bde50eeb077
“codedev-exp” <przemyslaw.truchan@pagepro.pl>
2019-04-18 16:54:56 +0200

    Button and Embedded Video in Generic content page

---
+++ src/features/genericContentPage/components/video/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const VideoContainer = styled.div`
+  position: relative;
+  padding-bottom: 56.25%;
+
+  iframe {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+  }
+`
