7f03a164472c41a82567f9253340e5c479fd945d
Sonya Moisset <sonya.moisset@gmail.com>
2019-04-20 00:40:09 +0100

    Extract mocks from navItem

--- src/components/navigation/navItem/_spec.js
+++ src/components/navigation/navItem/_spec.js
@@ -3,39 +3,7 @@ import { shallow, mount } from 'enzyme'
 import toJSON from 'enzyme-to-json'
 import 'jest-styled-components'
 import NavItem from '../navItem'
-
-const mockData = {
-  id: 'nav-learn',
-  title: 'Learn',
-  desc:
-    'Cupcake ipsum dolor sit. Amet dragée chocolate donut. Jelly-o chupa chups liquorice chocolate.',
-  url: '/',
-  submenu: [
-    {
-      heading: 'Jelly-o chupa chups',
-      links: [
-        { title: 'Cupcake ipsum dolor sit', url: '/' },
-        { title: 'Cupcake ipsum sit', url: '/' },
-        { title: 'Cupcake dolor sit', url: '/' },
-        { title: 'Cupcake ipsum dolor', url: '/' },
-      ],
-    },
-    {
-      heading: 'Icing gummi bears powder',
-      links: [
-        { title: 'Amet dragée donut', url: '/' },
-        { title: 'Amet dragée chocolate donut', url: '/' },
-      ],
-    },
-    {
-      heading: 'Bear claw pastry bonbon',
-      links: [
-        { title: 'Carrot cake fruitcake', url: '/' },
-        { title: 'Carrot cake muffin fruitcake', url: '/' },
-      ],
-    },
-  ],
-}
+import { mockData } from './_mocks'
 
 describe('Desktop version <NavItem/>', () => {
   beforeAll(() => {
