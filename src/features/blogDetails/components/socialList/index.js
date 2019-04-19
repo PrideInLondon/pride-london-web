7479d6d3bfa1ccde58b9f81e84f190bdb755f9b2
laij84 <jase_lai@hotmail.com>
2019-04-19 16:01:58 +0100

    fix social sharing links on blogposts (#318)

--- src/features/blogDetails/components/socialList/index.js
+++ src/features/blogDetails/components/socialList/index.js
@@ -4,33 +4,45 @@ import Twitter from '../../../../components/icons/twitter'
 import LinkedIn from '../../../../components/icons/linkedin'
 import { SocialList, SocialItem, SocialLink } from './styles'
 
-const SocialMediaList = () => (
-  <SocialList>
-    <SocialItem>
-      <SocialLink
-        href="https://twitter.com/PrideInLondon"
-        aria-label="Link to Twitter page"
-      >
-        <Twitter />
-      </SocialLink>
-    </SocialItem>
-    <SocialItem>
-      <SocialLink
-        href="https://www.facebook.com/pg/LondonLGBTPride"
-        aria-label="Link to Facebook page"
-      >
-        <Facebook />
-      </SocialLink>
-    </SocialItem>
-    <SocialItem>
-      <SocialLink
-        href="https://www.linkedin.com/company/prideinlondon"
-        aria-label="Link to LinkedIn page"
-      >
-        <LinkedIn />
-      </SocialLink>
-    </SocialItem>
-  </SocialList>
-)
+const SocialMediaList = () => {
+  let location
+  if (typeof window !== 'undefined') {
+    location = window.location.href
+  }
+  return (
+    <SocialList>
+      <SocialItem>
+        <SocialLink
+          href={`https://twitter.com/home?status=${location}`}
+          aria-label="Share on Twitter"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Twitter />
+        </SocialLink>
+      </SocialItem>
+      <SocialItem>
+        <SocialLink
+          href={`https://www.facebook.com/sharer/sharer.php?u=${location}`}
+          aria-label="Share on Facebook"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Facebook />
+        </SocialLink>
+      </SocialItem>
+      <SocialItem>
+        <SocialLink
+          href={`https://www.linkedin.com/shareArticle?mini=true&url=${location}`}
+          aria-label="Share on LinkedIn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedIn />
+        </SocialLink>
+      </SocialItem>
+    </SocialList>
+  )
+}
 
 export default SocialMediaList
