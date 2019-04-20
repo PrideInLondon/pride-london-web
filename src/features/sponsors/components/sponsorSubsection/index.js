1598c6fcbc2ae6e458463bb09d63e42b9c924544
Sonya Moisset <sonya.moisset@gmail.com>
2019-04-20 01:27:46 +0100

    Extract styles from sponsorSubsection

--- src/features/sponsors/components/sponsorSubsection/index.js
+++ src/features/sponsors/components/sponsorSubsection/index.js
@@ -1,34 +1,6 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import { media } from '../../../../theme/media'
-
-const BadgesContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  margin: 15px 0;
-
-  ${media.tablet`
-    margin: 20px 0;
-  `};
-
-  ${media.tabletMax`
-    margin-right: -10px;
-  `};
-`
-
-const TitleWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-
-  svg {
-    margin-right: 10px;
-  }
-`
-
-const Title = styled.h3`
-  margin: 0;
-`
+import { BadgesContainer, TitleWrapper, Title } from './styles'
 
 const SponsorsSubsection = ({ title, icon, children }) => (
   <Fragment>
