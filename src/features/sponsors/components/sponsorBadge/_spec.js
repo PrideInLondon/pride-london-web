57e8a1c9ecf3e03d29e654aa6ec9428f4d0f948b
Sonya Moisset <sonya.moisset@gmail.com>
2019-04-20 12:24:55 +0100

    [Refactoring] Sponsors features architecture (#320)
    * Extract mocks from sponsorBadge
    
    * Extract styles from sponsorBadge
    
    * Extract mocks from sponsorSubsection
    
    * Extract styles from sponsorSubsection

--- src/features/sponsors/components/sponsorBadge/_spec.js
+++ src/features/sponsors/components/sponsorBadge/_spec.js
@@ -2,16 +2,10 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import toJSON from 'enzyme-to-json'
 import SponsorBadge from '../sponsorBadge'
-
-const mockSponsor = {
-  level: 'Headline',
-  logo: '/logo1',
-  name: 'Sponsor One',
-  url: 'https://sponsor1.com',
-}
+import { mockSponsor } from './_mocks'
 
 describe('<SponsorBadge/>', () => {
-  it('Should render and match snapshot', () => {
+  it('renders the component <SponsorBadge />', () => {
     const wrapper = shallow(<SponsorBadge {...mockSponsor} />)
     expect(toJSON(wrapper)).toMatchSnapshot()
     expect(wrapper.prop('href')).toBe(mockSponsor.url)
--- src/features/sponsors/components/sponsorSubsection/_spec.js
+++ src/features/sponsors/components/sponsorSubsection/_spec.js
@@ -4,24 +4,10 @@ import toJSON from 'enzyme-to-json'
 import SponsorsSubsection from '../sponsorSubsection'
 import SponsorStar from '../../../../components/icons/sponsorStar'
 import renderSponsors from '../../helpers'
-
-const mockSponsors = [
-  {
-    level: 'Headline',
-    logo: '/logo1',
-    name: 'Sponsor One',
-    url: 'https://sponsor1.com',
-  },
-  {
-    level: 'Headline',
-    logo: '/logo2',
-    name: 'Sponsor Two',
-    url: 'https://sponsor2.com',
-  },
-]
+import { mockSponsors } from './_mocks'
 
 describe('<SponsorSubsection/>', () => {
-  it('Should render and match snapshot', () => {
+  it('renders the component <SponsorSubsection />', () => {
     const wrapper = shallow(
       <SponsorsSubsection title="Bronze sponsors" icon={<SponsorStar />}>
         {renderSponsors(mockSponsors)}
