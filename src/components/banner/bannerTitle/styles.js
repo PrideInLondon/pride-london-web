5716f1db254d6ddd73698cc73ed3772b805c0c5f
Jason Lai <jase_lai@hotmail.com>
2019-04-20 11:37:14 +0100

    fix banner titles line-height across breakpoints

--- src/components/banner/bannerTitle/styles.js
+++ src/components/banner/bannerTitle/styles.js
@@ -10,12 +10,13 @@ export const BannerTitleWrapper = styled.div`
 export const BannerTitle = styled.h1`
   margin: 0;
   display: inline;
-  box-shadow: 10px 0 0 ${theme.colors.white}, -10px 0 0 ${theme.colors.white};
+  box-shadow: 5px 0 0 5px ${theme.colors.white},
+    -5px 0 0 5px ${theme.colors.white};
   box-decoration-break: clone;
   background-color: white;
   color: ${theme.colors.indigo};
   font-size: 1.75rem;
-  line-height: 1.5;
+  line-height: 1.6;
 
   ${media.tablet`
     font-size: 3.5rem;
