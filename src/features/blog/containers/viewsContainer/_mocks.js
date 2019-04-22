f71fba375028eae006ac8e0e71391907178463f4
Lee Doughty <leedoughtydesign@gmail.com>
2019-04-22 16:11:50 +0100

    add more tests for views container

---
+++ src/features/blog/containers/viewsContainer/_mocks.js
@@ -0,0 +1,34 @@
+export const views = [
+  {
+    author: {
+      display_name: {
+        display_name: 'Test',
+      },
+    },
+    category: 'Views',
+    datePublished: '2019-05-17',
+    headerImage: {
+      file: {
+        url: 'src',
+      },
+    },
+    id: '1',
+    title: 'test',
+  },
+  {
+    author: {
+      display_name: {
+        display_name: 'Test',
+      },
+    },
+    category: 'Views',
+    datePublished: '2019-05-17',
+    headerImage: {
+      file: {
+        url: 'src',
+      },
+    },
+    id: '2',
+    title: 'test',
+  },
+]
