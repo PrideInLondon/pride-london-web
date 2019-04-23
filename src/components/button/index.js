fd4cabf9a7e39a871f6b0069b0630e6a50d5428a
Jason Lai <jase_lai@hotmail.com>
2019-04-23 09:26:06 +0100

    refactor button to use Gatsby Link if internal URL

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,62 +1,78 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { darken } from 'polished'
 import { media } from '../../theme/media'
 import theme from '../../theme/theme'
 
-export const Button = props => {
-  const StyledButton = styled[props.link ? 'a' : 'button']`
-    box-sizing: border-box;
-    padding: 12px 35px;
-    border-radius: 4px;
-    border: ${styleProps =>
-      styleProps.primary ? 'none' : `2px solid ${theme.colors.white}`};
-    display: inline-block;
-    text-align: center;
-    color: ${styleProps =>
-      styleProps.primary ? theme.colors.indigo : theme.colors.white};
+const buttonStyles = css`
+  box-sizing: border-box;
+  padding: 12px 35px;
+  border-radius: 4px;
+  border: ${styleProps =>
+    styleProps.primary ? 'none' : `2px solid ${theme.colors.white}`};
+  display: inline-block;
+  text-align: center;
+  color: ${styleProps =>
+    styleProps.primary ? theme.colors.indigo : theme.colors.white};
+  background-color: ${styleProps =>
+    styleProps.white
+      ? theme.colors.white
+      : styleProps.primary
+      ? theme.colors.eucalyptusGreen
+      : theme.colors.indigo};
+  font-family: ${theme.fonts.title};
+  font-weight: 700;
+  font-size: ${styleProps => (styleProps.small ? '0.875rem' : '1.125rem')};
+  line-height: 1.388;
+  min-width: ${styleProps => {
+    return styleProps.wide
+      ? !styleProps.flexwidth
+        ? '250px'
+        : 'unset'
+      : '180px'
+  }};
+  width: ${styleProps => (styleProps.fullmobile ? '100%' : 'auto')};
+  cursor: pointer;
+  text-decoration: none;
+  text-align: center;
+  transition: background-color 0.15s linear;
+
+  &:hover {
     background-color: ${styleProps =>
       styleProps.white
-        ? theme.colors.white
+        ? darken(0.1, theme.colors.white)
         : styleProps.primary
-        ? theme.colors.eucalyptusGreen
-        : theme.colors.indigo};
-    font-family: ${theme.fonts.title};
-    font-weight: 700;
-    font-size: ${styleProps => (styleProps.small ? '0.875rem' : '1.125rem')};
-    line-height: 1.388;
-    min-width: ${styleProps => {
-      return styleProps.wide
-        ? !styleProps.flexwidth
-          ? '250px'
-          : 'unset'
-        : '180px'
-    }};
-    width: ${styleProps => (styleProps.fullmobile ? '100%' : 'auto')};
-    cursor: pointer;
-    text-decoration: none;
-    text-align: center;
-    transition: background-color 0.15s linear;
+        ? darken(0.1, theme.colors.eucalyptusGreen)
+        : darken(0.1, theme.colors.indigo)};
+  }
 
-    &:hover {
-      background-color: ${styleProps =>
-        styleProps.white
-          ? darken(0.1, theme.colors.white)
-          : styleProps.primary
-          ? darken(0.1, theme.colors.eucalyptusGreen)
-          : darken(0.1, theme.colors.indigo)};
-    }
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
 
-    &:disabled {
-      opacity: 0.5;
-      cursor: not-allowed;
-    }
-
-    ${media.tablet`
+  ${media.tablet`
       width: auto;
-    `};
-  `
+  `};
+`
+export const Button = props => {
+  let StyledButton
+  const external = /^(http|https|ftp)/
+
+  if (props.link && props.to && !external.test(props.to)) {
+    StyledButton = styled(Link)`
+      ${buttonStyles}
+    `
+  } else if (props.link) {
+    StyledButton = styled.a`
+      ${buttonStyles}
+    `
+  } else {
+    StyledButton = styled.button`
+      ${buttonStyles}
+    `
+  }
 
   return (
     <StyledButton
