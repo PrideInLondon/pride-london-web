b67a8483dee99d748ae27c2ae69b532bf3ba083b
Jason Lai <jase_lai@hotmail.com>
2019-04-24 18:59:42 +0100

    remove console log

--- src/components/grid/index.js
+++ src/components/grid/index.js
@@ -43,25 +43,24 @@ const Row = props => (
   />
 )
 
-const Column = props =>
-  console.log(props) || (
-    <Box
-      {...props}
-      // Padding right/left. 0-4 represent index on the spacing scale. Numbers 5+ represents px value
-      px={
-        props.px !== null
-          ? props.px
-          : [
-              1, // pad btwn 0px and 1st breakpoint (375px). 1 = 5px on spacing scale
-              2, // pad btwn 1st breakpoint(375px) and 2nd breakpoint (768px). 2 = 10px on spacing scale
-              2, // pad btwn 2nd breakpoint(768px) and 3rd breakpoint (1280px). 2 = 10px on spacing scale
-              3, // pad from 3rd breakpoint(1024px) onwards
-            ]
-      }
-      // Padding top/bottom. 0-4 represent index on the spacing scale. Numbers 5+ represents px value
-      py={props.py !== null ? props.py : 2} // Global padding. 2 = 10px on spacing scale
-    />
-  )
+const Column = props => (
+  <Box
+    {...props}
+    // Padding right/left. 0-4 represent index on the spacing scale. Numbers 5+ represents px value
+    px={
+      props.px !== null
+        ? props.px
+        : [
+            1, // pad btwn 0px and 1st breakpoint (375px). 1 = 5px on spacing scale
+            2, // pad btwn 1st breakpoint(375px) and 2nd breakpoint (768px). 2 = 10px on spacing scale
+            2, // pad btwn 2nd breakpoint(768px) and 3rd breakpoint (1280px). 2 = 10px on spacing scale
+            3, // pad from 3rd breakpoint(1024px) onwards
+          ]
+    }
+    // Padding top/bottom. 0-4 represent index on the spacing scale. Numbers 5+ represents px value
+    py={props.py !== null ? props.py : 2} // Global padding. 2 = 10px on spacing scale
+  />
+)
 
 const FlexColumn = styled(Column)`
   display: block;
