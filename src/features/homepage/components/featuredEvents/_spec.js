f4812fb72264e47ae8be0cc547bb26b0b753edc4
laij84 <jase_lai@hotmail.com>
2019-05-03 16:05:38 +0100

    separate query from featured events component, update tests to mock data rendered

--- src/features/homepage/components/featuredEvents/_spec.js
+++ src/features/homepage/components/featuredEvents/_spec.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import toJSON from 'enzyme-to-json'
 import { FeaturedEventContent } from './styles'
+import { mockEvents } from './_mocks'
 import FeaturedEvents from '.'
 import 'jest-styled-components'
 
 describe(FeaturedEvents.name, () => {
   it('renders with default values', () => {
-    const wrapper = shallow(<FeaturedEvents />)
+    const wrapper = shallow(<FeaturedEvents events={mockEvents} />)
     expect(wrapper).toMatchSnapshot()
   })
 })
@@ -18,10 +19,3 @@ describe('Featured Event Content', () => {
     expect(toJSON(wrapper)).toMatchSnapshot()
   })
 })
-
-// describe(‘Button Component’, () => {
-//   it(‘renders a button element’, () => {
-//     const wrapper = shallow(Button);
-//     // this test is basically worthless
-//     expect(wrapper).toBeA(‘button’);
-//   });
--- src/features/homepage/components/featuredEvents/index.js
+++ src/features/homepage/components/featuredEvents/index.js
@@ -1,74 +1,32 @@
 import React from 'react'
 import Slider from 'react-slick'
-import { graphql, StaticQuery } from 'gatsby'
+import PropTypes from 'prop-types'
 import { EventListingCard } from '../../../../features/events'
 import { FlexColumn } from '../../../../components/grid'
 import { settings } from './slickSettings'
 import { FeaturedEventContent } from './styles'
 
-const query = graphql`
-  query featuredEventsQuery {
-    allContentfulEventsFeatured(filter: { title: { eq: "Home" } }) {
-      edges {
-        node {
-          id
-          title
-          events {
-            id
-            name
-            startTime
-            endTime
-            eventPriceLow
-            eventsListPicture {
-              title
-              file {
-                url
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
-
-const FeaturedEvents = () => (
+const FeaturedEvents = ({ events }) => (
   <FeaturedEventContent>
-    <StaticQuery
-      query={query}
-      render={({
-        allContentfulEventsFeatured: {
-          edges: [
-            {
-              node: { events },
-            },
-          ],
-        },
-      }) => {
-        return (
-          <Slider {...settings}>
-            {events
-              .map(node => {
-                return { node }
-              })
-              .map(event => (
-                <FlexColumn
-                  width={[
-                    1, // 100% between 0px screen width and first breakpoint (375px)
-                    1, // 100% between first breakpoint(375px) and second breakpoint (768px)
-                    1 / 2, // 50% between second breakpoint(768px) and third breakpoint (1024px)
-                    1 / 3, // 33% between third breakpoint(1280px) and fourth breakpoint (1440px)
-                  ]}
-                  key={`${event.node.id}${event.node.name}`}
-                >
-                  <EventListingCard displaycolumn event={event.node} />
-                </FlexColumn>
-              ))}
-          </Slider>
-        )
-      }}
-    />
+    <Slider {...settings}>
+      {events.map(event => (
+        <FlexColumn
+          width={[
+            1, // 100% between 0px screen width and first breakpoint (375px)
+            1, // 100% between first breakpoint(375px) and second breakpoint (768px)
+            1 / 2, // 50% between second breakpoint(768px) and third breakpoint (1024px)
+            1 / 3, // 33% between third breakpoint(1280px) and fourth breakpoint (1440px)
+          ]}
+          key={`${event.node.id}${event.node.name}`}
+        >
+          <EventListingCard displaycolumn event={event.node} />
+        </FlexColumn>
+      ))}
+    </Slider>
   </FeaturedEventContent>
 )
 
+FeaturedEvents.propTypes = {
+  events: PropTypes.array.isRequired,
+}
 export default FeaturedEvents
