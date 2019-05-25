004a211dfac97baf8ddfe4eb8c2fb59023d8c056
Giancarlo Galliani <galliani.08@gmail.com>
2019-05-25 17:34:55 +0100

    Add groups/navigation to groups for each letter (#488)
    * Add groups/navigation to groups for each letter
    
    * Tweak styles
    
    * Fix import order

--- src/features/paradeGroups/containers/paradeGroupsContainer/styles.js
+++ src/features/paradeGroups/containers/paradeGroupsContainer/styles.js
@@ -1,9 +1,19 @@
 import styled from 'styled-components'
+import theme from '../../../../theme/theme'
 
 export const LetterContainer = styled.div`
+  align-items: center;
+  background-color: ${theme.colors.white};
   display: flex;
-  height: 25px;
+  height: 50px;
   justify-content: space-between;
   overflow-x: scroll;
   margin-bottom: 20px;
+  position: sticky;
+  top: 0;
+`
+
+export const ScrollingContainer = styled.div`
+  overflow: scroll;
+  height: 500px;
 `
