37ca7d415660c44eff9cc51d4a316e8bba6bcc96
Giancarlo Galliani <galliani.08@gmail.com>
2019-05-31 12:23:52 +0100

    Fix test warning

--- src/features/paradeGroups/containers/paradeGroupsContainer/_spec.js
+++ src/features/paradeGroups/containers/paradeGroupsContainer/_spec.js
@@ -4,7 +4,10 @@ import ParadeGroupsContainer from '.'
 
 describe(ParadeGroupsContainer.name, () => {
   it('should render correctly', () => {
-    const paradeGroups = [{ name: 'group 1' }, { name: 'group 2' }]
+    const paradeGroups = [
+      { name: 'group 1', id: '1' },
+      { name: 'group 2', id: '2' },
+    ]
     const wrapper = shallow(
       <ParadeGroupsContainer paradeGroups={paradeGroups} />
     )
