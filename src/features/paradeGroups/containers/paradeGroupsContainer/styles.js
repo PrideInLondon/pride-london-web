10b1e1b228e5f060965e9aee7aaff5374eda173d
Rafał Gołubowicz <rafalgolubowicz@MBP-Rafa.lan>
2019-06-10 15:44:39 +0200

    Fix alphabet letters and groups sizes

--- src/features/paradeGroups/containers/paradeGroupsContainer/styles.js
+++ src/features/paradeGroups/containers/paradeGroupsContainer/styles.js
@@ -1,16 +1,26 @@
 import styled from 'styled-components'
 import theme from '../../../../theme/theme'
 
+import { media } from '../../../../theme/media'
+
 export const LetterContainer = styled.div`
   align-items: center;
   background-color: ${theme.colors.white};
   display: flex;
   height: 50px;
-  justify-content: space-between;
-  overflow-x: scroll;
-  margin-bottom: 20px;
+  overflow-x: hidden;
   position: sticky;
   top: 0;
+  padding: 0 20px;
 
-  padding: 0px 20px;
+  ${media.tablet`
+    padding: 0;
+  `}
+`
+
+export const ScrolledLetters = styled.div`
+  width: 100%;
+  overflow-x: auto;
+  display: flex;
+  justify-content: space-between;
 `
