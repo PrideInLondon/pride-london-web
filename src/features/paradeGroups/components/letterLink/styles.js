3eca45039c64538e82d47c59b13563314afeda55
rgolubowicz <51472746+rgolubowicz@users.noreply.github.com>
2019-06-11 13:02:35 +0200

    Fix alphabet letters and groups sizes (#542)

--- src/features/paradeGroups/components/letterLink/styles.js
+++ src/features/paradeGroups/components/letterLink/styles.js
@@ -1,15 +1,27 @@
 import styled from 'styled-components'
 import theme from '../../../../theme/theme'
+import { media } from '../../../../theme/media'
 
 const StyledLetterLink = styled.a`
   color: ${props => (props.isDisabled ? 'grey' : 'initial')};
   font-size: 16px;
   margin-right: 8px;
   text-decoration: none;
+  border-bottom: 2px solid transparent;
 
   &:hover {
     border-bottom: 2px solid ${theme.colors.eucalyptusGreen};
+    font-weight: 600;
+    color: ${theme.colors.indigo};
   }
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+
+  ${media.tablet`
+    font-size: 18px;
+  `}
 `
 
 export default StyledLetterLink
