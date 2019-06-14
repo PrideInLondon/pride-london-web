0ebb67f605ada672d6d0c9ae6cebf97d800886f9
rgolubowicz <51472746+rgolubowicz@users.noreply.github.com>
2019-06-14 03:03:25 +0200

    Spacing between parade groups and headers (#556)
    * Spacing between parade groups and headers
    
    * mobile parade groups spacing

--- src/features/paradeGroups/components/paradeGroup/styles.js
+++ src/features/paradeGroups/components/paradeGroup/styles.js
@@ -6,13 +6,12 @@ export const RowContainer = styled.div`
   border-bottom: 1px solid ${theme.colors.lightGrey};
   display: flex;
   flex-direction: row;
-  min-height: 80px;
   overflow-x: auto;
   overflow-y: hidden;
-  padding: 0 20px;
+  padding: 15px 20px;
 
   ${media.tablet`
-    padding: 0;
+    padding: 17px 0 23px 0;
   `}
 `
 
