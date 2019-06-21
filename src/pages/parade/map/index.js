12e0aeb0f87b70df6886de934d88c2e158d14550
Rafal Golubowicz <rafal.golubowicz@pagepro.pl>
2019-06-21 13:35:49 +0200

    links fixes

--- src/pages/parade/map/index.js
+++ src/pages/parade/map/index.js
@@ -6,7 +6,7 @@ import googlePlayBadge from '../../../theme/assets/images/google-play.svg'
 import { Column, Row } from '../../../components/grid'
 import theme from '../../../theme/theme'
 import { media } from '../../../theme/media'
-// import Button from '../../../components/button'
+import Button from '../../../components/button'
 
 const ParadeMapContainer = styled(Row)`
   font-family: Poppins;
@@ -75,30 +75,30 @@ const Map = styled.iframe`
   `}
 `
 
-// const DownloadPDFButton = styled(Button)`
-//   ${media.tabletMax`
-//     display: none;
-//   `}
-// `
+const DownloadPDFButton = styled(Button)`
+  ${media.tabletMax`
+    display: none;
+  `}
+`
 
-// const DownloadPDFLinkMobile = styled.a`
-//   font-family: Roboto;
-//   font-size: 16px;
-//   line-height: 1.38;
-//   color: ${theme.colors.black};
-//   text-decoration: none;
-//   border-bottom: 2px solid ${theme.colors.eucalyptusGreen};
-//   transition: color 0.15s linear;
-//   cursor: pointer;
-//   margin-top: 30px;
-//   &:hover,
-//   &:focus {
-//     color: ${theme.colors.eucalyptusGreen};
-//   }
-//   ${media.tablet`
-//     display: none;
-//   `}
-// `
+const DownloadPDFLinkMobile = styled.a`
+  font-family: Roboto;
+  font-size: 16px;
+  line-height: 1.38;
+  color: ${theme.colors.black};
+  text-decoration: none;
+  border-bottom: 2px solid ${theme.colors.eucalyptusGreen};
+  transition: color 0.15s linear;
+  cursor: pointer;
+  margin-top: 30px;
+  &:hover,
+  &:focus {
+    color: ${theme.colors.eucalyptusGreen};
+  }
+  ${media.tablet`
+    display: none;
+  `}
+`
 
 const BottomSection = styled.div`
   padding-bottom: 50px;
@@ -136,78 +136,91 @@ const AppDownloadButton = styled.a`
   `}
 `
 
-const ParadeMap = () => (
-  <Fragment>
-    <ParadeMapContainer mx={0}>
-      <Column
-        width={[1, 1, 0.5, 0.45, 0.4]}
-        pt={[60, 60, 0]}
-        pb={0}
-        pl={[20, 20, 20, 30, 90]}
-        pr={20}
-      >
-        <ParadeMapContent>
-          <div style={{ marginTop: 'auto' }}>
-            <Title>Parade map</Title>
-            <Subtitle>
-              Use it to find the best places to watch the parade and navigate to
-              the different stages & areas.
-            </Subtitle>
-            {/* <DownloadPDFButton link primary to="">
-              Download map as a PDF
-            </DownloadPDFButton> */}
-          </div>
-          <BottomSection>
-            <DownloadAppLabel>
-              Download our iOS & Android app to carry the map around with you,
-              internet connection is poor on the day.
-            </DownloadAppLabel>
-            <AppDownloadButtons>
-              <AppDownloadButton
-                href="https://itunes.apple.com/gb/app/pride-in-london/id1250496471"
-                aria-label="Download from App Store"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  alt="Pride in London on the App Store"
-                  src={appStoreBadge}
-                />
-              </AppDownloadButton>
-              <AppDownloadButton
-                href="https://play.google.com/store/apps/details?id=org.prideinlondon.festival&hl=en"
-                aria-label="Download from Google Play"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  alt="Pride in London in the Google Play"
-                  src={googlePlayBadge}
-                />
-              </AppDownloadButton>
-            </AppDownloadButtons>
-            {/* <DownloadPDFLinkMobile href="">
-              Download map as a PDF
-            </DownloadPDFLinkMobile> */}
-          </BottomSection>
-        </ParadeMapContent>
-      </Column>
-      <Column width={[1, 1, 0.5, 0.55, 0.6]} pt={[0]} px={0} pb={0}>
-        <Map
-          frameBorder="0"
-          src={`https://www.google.com/maps/d/embed?${querystring.encode({
-            mid: `1I5M9Oyc1damFcMhsi6yfODAMD6NXeIxi`,
-            z: 16,
-            ll: `51.51004, -0.13501`,
-            hl: `en`,
-            output: `embed`,
-            t: 'm',
-          })}`}
-          allowFullScreen
-        ></Map>
-      </Column>
-    </ParadeMapContainer>
-  </Fragment>
-)
+const ParadeMap = () => {
+  const paradeMapID = '1I5M9Oyc1damFcMhsi6yfODAMD6NXeIxi'
+  const googlePlayUrl =
+    'https://play.google.com/store/apps/details?id=org.prideinlondon.festival&hl=en'
+  const appStoreUrl =
+    'https://itunes.apple.com/gb/app/pride-in-london/id1250496471'
+  const mapPdfLink = null
 
+  return (
+    <Fragment>
+      <ParadeMapContainer mx={0}>
+        <Column
+          width={[1, 1, 0.5, 0.45, 0.4]}
+          pt={[60, 60, 0]}
+          pb={0}
+          pl={[20, 20, 20, 30, 90]}
+          pr={20}
+        >
+          <ParadeMapContent>
+            <div style={{ marginTop: 'auto' }}>
+              <Title>Parade map</Title>
+              <Subtitle>
+                Use it to find the best places to watch the parade and navigate
+                to the different stages & areas.
+              </Subtitle>
+              {mapPdfLink && (
+                <DownloadPDFButton link primary to={mapPdfLink}>
+                  Download map as a PDF
+                </DownloadPDFButton>
+              )}
+            </div>
+            <BottomSection>
+              <DownloadAppLabel>
+                Download our iOS & Android app to carry the map around with you,
+                internet connection is poor on the day.
+              </DownloadAppLabel>
+              <AppDownloadButtons>
+                <AppDownloadButton
+                  href={appStoreUrl}
+                  aria-label="Download from App Store"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    alt="Pride in London on the App Store"
+                    src={appStoreBadge}
+                  />
+                </AppDownloadButton>
+                <AppDownloadButton
+                  href={googlePlayUrl}
+                  aria-label="Download from Google Play"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    alt="Pride in London in the Google Play"
+                    src={googlePlayBadge}
+                  />
+                </AppDownloadButton>
+              </AppDownloadButtons>
+              {mapPdfLink && (
+                <DownloadPDFLinkMobile href={mapPdfLink}>
+                  Download map as a PDF
+                </DownloadPDFLinkMobile>
+              )}
+            </BottomSection>
+          </ParadeMapContent>
+        </Column>
+        <Column width={[1, 1, 0.5, 0.55, 0.6]} pt={[0]} px={0} pb={0}>
+          <Map
+            frameBorder="0"
+            src={`https://www.google.com/maps/d/embed?${querystring.encode({
+              mid: paradeMapID,
+              z: 16,
+              ll: `51.51004, -0.13501`,
+              hl: `en`,
+              output: `embed`,
+              t: 'm',
+              style: 'feature:poi|visibility:off',
+            })}`}
+            allowFullScreen
+          ></Map>
+        </Column>
+      </ParadeMapContainer>
+    </Fragment>
+  )
+}
 export default ParadeMap
