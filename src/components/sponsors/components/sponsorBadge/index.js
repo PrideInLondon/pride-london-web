554dd0feb77f260fa0ec91e3458fea7e586ffb02
Rafal Golubowicz <rafal.golubowicz@pagepro.pl>
2019-06-25 09:29:12 +0200

    Sponsor sections in generic content pages

--- src/components/sponsors/components/sponsorBadge/index.js
+++ src/components/sponsors/components/sponsorBadge/index.js
@@ -3,10 +3,21 @@ import PropTypes from 'prop-types'
 import constants from '../../../../constants'
 import { Badge, BadgeImage, NamePlaceholder } from './styles'
 
-const SponsorBadge = ({ logo, name, url, level }) => (
-  <Badge level={level} href={url} rel="noopener noreferrer" target="_blank">
+const SponsorBadge = ({ logo, name, url, level, preventLevelSize }) => (
+  <Badge
+    level={level}
+    href={url}
+    rel="noopener noreferrer"
+    target="_blank"
+    preventLevelSize={preventLevelSize}
+  >
     {logo ? (
-      <BadgeImage src={logo} alt={name} level={level} />
+      <BadgeImage
+        src={logo}
+        alt={name}
+        level={level}
+        preventLevelSize={preventLevelSize}
+      />
     ) : (
       <NamePlaceholder>{name}</NamePlaceholder>
     )}
@@ -22,6 +33,11 @@ SponsorBadge.propTypes = {
       key => constants.sponsorLevels[key]
     )
   ).isRequired,
+  preventLevelSize: PropTypes.bool,
+}
+
+SponsorBadge.defaultProps = {
+  preventLevelSize: false,
 }
 
 export default SponsorBadge
