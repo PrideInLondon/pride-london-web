2dd471ef67afc20e7d13a296d59e8f4fe07c7e79
Rafal Golubowicz <rafal.golubowicz@pagepro.pl>
2019-07-09 09:42:34 +0200

    hide download app section, app prompts

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,6 @@ import BannerImg from '../theme/assets/images/banners/home/bg@3x.jpg'
 import LatestNewsContainer from '../features/homepage/containers/latestNewsContainer'
 import AnnouncementsContainer from '../features/homepage/containers/announcementsContainer'
 import DonateContainer from '../features/homepage/containers/donateContainer'
-import DownloadAppBox from '../features/homepage/components/downloadAppBox'
 
 const FeaturedEventsContainer = styled.div`
   background-color: #282a80;
@@ -213,7 +212,6 @@ const Home = ({
           </StyledColumn>
         </StyledRow>
       </StyledContainer>
-      <DownloadAppBox />
       <AnnouncementsContainer />
       <DonateContainer />
       <LatestNewsContainer />
