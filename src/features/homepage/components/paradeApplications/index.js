4cba9ddb3e26def5cb2aadcb469cd552fd82ba24
Em McDonald <mcdonald.emily@live.co.uk>
2020-01-19 13:13:26 +0000

    WEBNEW-41 :lipstick: Added mobile design for parade apps component

--- src/features/homepage/components/paradeApplications/index.js
+++ src/features/homepage/components/paradeApplications/index.js
@@ -2,13 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 import Button from '../../../../components/button'
 import { colors } from '../../../../theme/colors'
+import { mediaQueries } from '../../../../theme/mediaQueries'
 import Link from '../../../../components/ctaLink'
 import paradeApplicationsBg from '../../../../theme/assets/images/parade-applications-bg.jpg'
 
 const Container = styled.div`
   display: flex;
-  flex-direction: row;
-  margin-left: 90px;
+  flex-direction: column;
+  margin: 20px;
+
+  ${mediaQueries.md} {
+    flex-direction: row;
+    margin-left: 90px;
+    margin-top: 100px;
+  }
 `
 
 const BackgroundImage = styled.div`
@@ -17,31 +24,51 @@ const BackgroundImage = styled.div`
   background-repeat: no-repeat;
   background-position: center center;
   width: 100%;
-  height: 615px;
-  border-radius: 7px;
+
+  height: 300px;
+
+  ${mediaQueries.md} {
+    height: 615px;
+  }
 `
 
 const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-evenly;
   background-color: ${colors.white};
   text-align: left;
   border-radius: 7px;
-  width: 50%;
-  padding: 32px;
-  margin: 48px 48px 48px -128px;
+
+  width: 100%;
+  padding: 32px 0;
+
+  ${mediaQueries.md} {
+    margin: 48px 48px 48px -128px;
+    width: 50%;
+    padding: 32px;
+  }
 `
 
 const StyledDescription = styled.p`
-  color: ${colors.indigo};
   margin-top: 15px;
   margin-bottom: 40px;
   font-size: 22px;
+
+  color: ${colors.black};
+
+  ${mediaQueries.md} {
+    color: ${colors.indigo};
+  }
 `
 
 const StyledButton = styled(Button)`
   margin-bottom: 30px;
-  margin-right: 64px;
+  margin-right: 128px;
+
+  ${mediaQueries.md} {
+    margin-right: 64px;
+  }
 `
 
 const StyledLink = styled(Link)`
