b4ade5b35fb55528c77e9875b2bc4493f8e79829
Jason Lai <jase_lai@hotmail.com>
2020-01-19 19:08:39 +0000

    add card knob and update storybook config, apply withKnobs decorator globally

--- src/components/card/Card.stories.tsx
+++ src/components/card/Card.stories.tsx
@@ -1,53 +1,42 @@
 import React from 'react'
 import styled from 'styled-components'
 import { storiesOf } from '@storybook/react'
-import { withKnobs } from '@storybook/addon-knobs'
+import { select } from '@storybook/addon-knobs'
 import { Card, CardImage, CardContent, CardTitle, CardFooter } from './Card'
 import umbrellaJpg from './umbrella.jpg'
+
+const variants = {
+  Column: 'column',
+  Row: 'row',
+} as const
 
 const CardWrapper = styled.div`
   display: flex;
   max-width: 400px;
 `
 
-storiesOf('Card', module)
-  .addDecorator(withKnobs)
-  .add(
-    'with image',
-    () => {
-      return (
-        <CardWrapper>
-          <Card>
-            <CardImage
-              image={{
-                width: 200,
-                height: 200,
-                src: umbrellaJpg,
-                srcSet: umbrellaJpg,
-              }}
-              alt="A rainbow umbrella"
-            />
-            <CardContent height={{ desktop: '130px' }}>
-              <CardTitle>This is the title</CardTitle>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-              <p>
-                Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim id est laborum.
-              </p>
-              <CardFooter>This is the footer</CardFooter>
-            </CardContent>
-          </Card>
-        </CardWrapper>
-      )
-    },
-    {
-      info: { inline: true },
-    }
+storiesOf('Card', module).add('with image', () => {
+  return (
+    <CardWrapper>
+      <Card variant={select('Variant', variants, variants.Column)}>
+        <CardImage
+          image={{
+            width: 200,
+            height: 200,
+            src: umbrellaJpg,
+            srcSet: umbrellaJpg,
+          }}
+          alt="A rainbow umbrella"
+        />
+        <CardContent>
+          <CardTitle>This is the title</CardTitle>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <CardFooter>This is the footer</CardFooter>
+        </CardContent>
+      </Card>
+    </CardWrapper>
   )
+})
