59563563b5c0f9bff8495a39bf11aeaef1589a00
TP <thomas.price@acresoftware.com>
2020-01-24 16:08:45 +0000

    WEBREF-16 Removed snapshot props test

--- src/components/input/Input.test.js
+++ src/components/input/Input.test.js
@@ -14,12 +14,6 @@ describe('<Input/>', () => {
       <Input id={id} label={label} type={type} required={required} />
     )
     expect(toJSON(component)).toMatchSnapshot()
-    expect(component.find('Inputstyles__StyledInput').prop('id')).toBe(id)
-    expect(component.find('Inputstyles__StyledInput').prop('required')).toBe(
-      required
-    )
-    expect(component.find('Inputstyles__StyledInput').prop('type')).toBe(type)
-    expect(component.find('Inputstyles__Label').text()).toBe(label)
   })
 
   it('should invoke onChange when triggered', () => {
@@ -30,12 +24,7 @@ describe('<Input/>', () => {
   })
 
   it('should change label focused prop on focus and blur of input', () => {
-    const id = 'InputId'
-    const label = 'Input field'
-    const type = 'text'
-    const component = shallow(
-      <Input label={label} type={type} id={id} required />
-    )
+    const component = shallow(<Input />)
 
     component.find('Inputstyles__StyledInput').simulate('focus')
     expect(component.find('Inputstyles__Label').props().focused).toBe(true)
