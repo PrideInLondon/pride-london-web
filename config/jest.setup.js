0591138ca6bdfcbf33d4f1c19a5c7e605712735e
Jason Lai <jase_lai@hotmail.com>
2020-01-25 16:06:15 +0000

    setup react testing library, add type def for styled-components theme for intellisense

--- config/jest.setup.js
+++ config/jest.setup.js
@@ -1,5 +1,5 @@
-const Enzyme = require('enzyme')
-const Adapter = require('enzyme-adapter-react-16')
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
 window.matchMedia =
   window.matchMedia ||
