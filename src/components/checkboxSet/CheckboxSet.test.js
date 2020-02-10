008153bc0e1c09c1c11d3e0eebc24b4202a54a29
egmcdonald <mcdonald.emily@live.co.uk>
2020-02-10 13:04:40 +0000

    WEBREF-47 :fire: Removed jest-styled-components import
    This came from a merge of master upstream

--- src/components/checkboxSet/CheckboxSet.test.js
+++ src/components/checkboxSet/CheckboxSet.test.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { mount } from 'enzyme'
-import 'jest-styled-components'
 import constants from '../../constants'
 import Checkbox from '../checkbox'
 import { Provider } from '../appContext'
