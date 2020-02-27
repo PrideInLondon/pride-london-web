fa69745f078041fda41ca189c8b046f67ee441ed
egmcdonald <mcdonald.emily@live.co.uk>
2020-02-27 19:01:17 +0000

    WEBREF-32 :truck: Moved home page to /homepage

---
+++ src/homepage/HomePage.js
@@ -0,0 +1,54 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import theme from '../theme/theme'
+import { colors } from '../theme/colors'
+import { mediaQueries } from '../theme/mediaQueries'
+import { Banner } from '../components/banner'
+import BannerImg from '../theme/assets/images/banners/home/bg@3x.jpg'
+import VolunteerBoxInfo from './VolunteerBoxInfo'
+import LatestNewsContainer from './LatestNewsContainer'
+import Announcements from './Announcements'
+import Donate from './Donate'
+
+const Divider = styled.hr`
+  margin: 64px 20px 20px 20px;
+  color: ${colors.mediumGrey};
+  height: 1px;
+
+  ${mediaQueries.md} {
+    display: none;
+  }
+`
+
+const HomePage = ({
+  data: {
+    file: { childImageSharp },
+  },
+}) => (
+  <Fragment>
+    <Banner
+      titleText="Pride in London"
+      subtitleText="The UK’s biggest, most diverse Pride. A home for every part of London’s LGBT+ community."
+      titlePosition="bottom"
+      date="Saturday 27 June 2020"
+      imageSrc={BannerImg}
+      color={theme.colors.eucalyptusGreen}
+      large
+      allowContentUnderflow
+      videoId="348603446"
+      fixed={childImageSharp}
+    />
+    <Divider />
+    <Donate />
+    <VolunteerBoxInfo />
+    <Announcements />
+    <LatestNewsContainer />
+  </Fragment>
+)
+
+HomePage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default HomePage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,7 @@
-import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
-import styled from 'styled-components'
-import theme from '../theme/theme'
-import { colors } from '../theme/colors'
-import { mediaQueries } from '../theme/mediaQueries'
-import { Banner } from '../components/banner'
-import VolunteerBoxInfo from '../homepage/VolunteerBoxInfo'
-import BannerImg from '../theme/assets/images/banners/home/bg@3x.jpg'
-import LatestNewsContainer from '../homepage/LatestNewsContainer'
-import Announcements from '../homepage/Announcements'
-import Donate from '../homepage/Donate'
+import { HomePage } from '../homepage'
 
-export const HomePageQuery = graphql`
+export const query = graphql`
   query HomePageQuery {
     file(relativePath: { regex: "/video-poster/" }) {
       size
@@ -32,44 +21,4 @@ export const HomePageQuery = graphql`
   }
 `
 
-const Divider = styled.hr`
-  margin: 64px 20px 20px 20px;
-  color: ${colors.mediumGrey};
-  height: 1px;
-
-  ${mediaQueries.md} {
-    display: none;
-  }
-`
-
-const Home = ({
-  data: {
-    file: { childImageSharp },
-  },
-}) => (
-  <Fragment>
-    <Banner
-      titleText="Pride in London"
-      subtitleText="The UK’s biggest, most diverse Pride. A home for every part of London’s LGBT+ community."
-      titlePosition="bottom"
-      date="Saturday 27 June 2020"
-      imageSrc={BannerImg}
-      color={theme.colors.eucalyptusGreen}
-      large
-      allowContentUnderflow
-      videoId="348603446"
-      fixed={childImageSharp}
-    />
-    <Divider />
-    <Donate />
-    <VolunteerBoxInfo />
-    <Announcements />
-    <LatestNewsContainer />
-  </Fragment>
-)
-
-Home.propTypes = {
-  data: PropTypes.object.isRequired,
-}
-
-export default Home
+export default HomePage
