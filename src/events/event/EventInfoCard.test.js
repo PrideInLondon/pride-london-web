a8bf20cfe4f95d8e8e52a210fb6a4795292de61b
Em McDonald <mcdonald.emily@live.co.uk>
2020-04-11 13:33:40 +0100

    WEBNEW-106 :fire: Removed unneccessary tests

--- src/events/event/EventInfoCard.test.js
+++ src/events/event/EventInfoCard.test.js
@@ -1,29 +1,4 @@
-import React from 'react'
-import { mount } from 'enzyme'
-import { mockEvents } from '../__mocks__'
-import EventInfoCard, { formatDayRange } from './EventInfoCard'
-
-describe(EventInfoCard.name, () => {
-  it('renders correctly', () => {
-    const wrapper = mount(<EventInfoCard {...mockEvents[0]} />)
-    expect(
-      wrapper
-        .find('EventInfoCard__Title')
-        .first()
-        .text()
-    ).toBe('Friday 2 June 2006 to Saturday 3 June 2006')
-  })
-
-  it('1 day event renders correctly', () => {
-    const wrapper = mount(<EventInfoCard {...mockEvents[1]} />)
-    expect(
-      wrapper
-        .find('EventInfoCard__Title')
-        .first()
-        .text()
-    ).toBe('Friday 2 June 2006')
-  })
-})
+import { formatDayRange } from './EventInfoCard'
 
 describe('formatDayRange', () => {
   it('should render a range of dates if the event is recurring', () => {
