625de33023bc4fbf2ea81dc9996569aaee9e45ce
schazbot <charlotte.pearce1984@gmail.com>
2020-05-11 11:43:54 +0100

    WEBREF-22 :recycle: changed to create a ref Div instead of using state to hold clientX and clientY

--- src/blog/ViewsContainer.js
+++ src/blog/ViewsContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { createRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import Slider from 'react-slick'
 import { Container, Row, Column } from '../components/grid'
@@ -6,43 +6,46 @@ import ViewsCard from './ViewsCard'
 import { CardContainer, ViewsTitle } from './ViewsContainer.styles'
 import { settings } from './slickSettings'
 
+let firstClientX, clientX
+
+const preventTouch = e => {
+  const minValue = 5 // threshold
+
+  clientX = e.touches[0].clientX - firstClientX
+
+  // Vertical scrolling does not work when you start swiping horizontally.
+  if (Math.abs(clientX) > minValue) {
+    e.returnValue = false
+    return false
+  }
+}
+
+const touchStart = e => (firstClientX = e.touches[0].clientX)
+
 const ViewsContainer = ({ views }) => {
   // Methods here to prevent horizontal scrolling
   // while swiping with react-slick, see
   // https://github.com/akiran/react-slick/issues/1240
 
-  const [firstClientX, setFirstClientX] = useState()
-  const [firstClientY, setFirstClientY] = useState()
-  const [clientX, setClientX] = useState()
+  const containerRef = createRef()
 
   useEffect(() => {
-    const touchStart = e => {
-      setFirstClientX(e.touches[0].clientX)
-      setFirstClientY(e.touches[0].clientY)
-    }
-
-    const preventTouch = e => {
-      const minValue = 5 // threshold
-
-      setClientX(e.touches[0].clientX - firstClientX)
-
-      // Vertical scrolling does not work when you start swiping horizontally.
-      if (Math.abs(clientX) > minValue) {
-        e.preventDefault()
-        e.returnValue = false
-        return false
-      }
-    }
-
-    window.addEventListener('touchstart', touchStart)
-    window.addEventListener('touchmove', preventTouch, { passive: false })
-    return () => {
-      window.removeEventListener('touchstart', touchStart)
-      window.removeEventListener('touchmove', preventTouch, {
+    if (containerRef.current) {
+      containerRef.current.addEventListener('touchstart', touchStart)
+      containerRef.current.addEventListener('touchmove', preventTouch, {
         passive: false,
       })
     }
-  }, [clientX, firstClientX, firstClientY])
+
+    return () => {
+      if (containerRef.current) {
+        containerRef.current.removeEventListener('touchstart', touchStart)
+        containerRef.current.removeEventListener('touchmove', preventTouch, {
+          passive: false,
+        })
+      }
+    }
+  })
 
   return (
     <section>
@@ -53,11 +56,13 @@ const ViewsContainer = ({ views }) => {
               <ViewsTitle isLight isCentered>
                 Views
               </ViewsTitle>
-              <Slider {...settings}>
-                {views.map(view => (
-                  <ViewsCard {...view} key={view.id} />
-                ))}
-              </Slider>
+              <div ref={containerRef}>
+                <Slider {...settings}>
+                  {views.map(view => (
+                    <ViewsCard {...view} key={view.id} />
+                  ))}
+                </Slider>
+              </div>
             </CardContainer>
           </Column>
         </Row>
