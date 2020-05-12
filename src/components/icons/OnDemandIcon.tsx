3981c4bb66ba0841f60d27e4b9f54d5504b76f4a
Em McDonald <mcdonald.emily@live.co.uk>
2020-05-12 22:12:31 +0100

    WEBNEW-118 :rotating_light: Updated SVG props for on demand

--- src/components/icons/OnDemandIcon.tsx
+++ src/components/icons/OnDemandIcon.tsx
@@ -13,20 +13,20 @@ const OnDemandIcon = ({ variant }: IconProps) => {
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M27 8H5V24H27V8ZM3 6V26H29V6H3Z"
         fill={fill}
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M20 16L14 20L14 12L20 16Z"
         fill="#2CDA9D"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M22 16L13 22L13 10L22 16ZM14.9286 13.4286L14.9286 18.5714L18.7857 16L14.9286 13.4286Z"
         fill={fill}
       />
