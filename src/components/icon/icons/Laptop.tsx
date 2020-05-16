00cd46d2296817862a8b7f1a6580935839fc1e85
Em McDonald <mcdonald.emily@live.co.uk>
2020-05-16 18:20:14 +0100

    WEBREF-73 :art: Cleaned up Laptop component name

--- src/components/icon/icons/Laptop.tsx
+++ src/components/icon/icons/Laptop.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { InternalIconProps } from '../Icon.types'
 
-const LaptopIcon = ({ colors: { primary } }: InternalIconProps) => (
+const Laptop = ({ colors: { primary } }: InternalIconProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="32"
@@ -28,4 +28,4 @@ const LaptopIcon = ({ colors: { primary } }: InternalIconProps) => (
   </svg>
 )
 
-export default LaptopIcon
+export default Laptop
