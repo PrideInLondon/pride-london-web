165db6f5b881a6bf22dc1a42ecb9ebef4728cbd4
Em McDonald <mcdonald.emily@live.co.uk>
2020-05-17 18:34:09 +0100

    WEBREF-73 :sparkles: Added story to show icon alignment with text

--- src/components/icon/Icon.stories.tsx
+++ src/components/icon/Icon.stories.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
 import { storiesOf } from '@storybook/react'
 import { select } from '@storybook/addon-knobs'
 import { colors } from '../../theme/colors'
@@ -8,21 +7,44 @@ import { COMPONENTS } from './Icon.utils'
 
 const names: string[] = Object.keys(COMPONENTS)
 
-const Wrapper = styled.div`
-  display: flex;
-`
-
-const Column = styled.div`
-  padding: 32px;
-`
-
-storiesOf('Icon', module).add('with variants', () => (
-  <Wrapper>
-    <Column style={{ backgroundColor: colors.white }}>
-      <Icon name={select('Name', names, names[0])} variant="indigo" />
-    </Column>
-    <Column style={{ backgroundColor: colors.indigo }}>
-      <Icon name={select('Name', names, names[0])} variant="white" />
-    </Column>
-  </Wrapper>
-))
+storiesOf('Icon', module)
+  .add('variants', () => {
+    const name: string = select('Name', names, names[0])
+    return (
+      <div style={{ display: 'flex', fontSize: '2em' }}>
+        <div
+          style={{
+            backgroundColor: colors.white,
+            color: colors.indigo,
+            padding: '1em',
+          }}
+        >
+          <Icon name={name} variant="indigo" />
+        </div>
+        <div
+          style={{
+            backgroundColor: colors.indigo,
+            color: colors.white,
+            padding: '1em',
+          }}
+        >
+          <Icon name={name} variant="white" />
+        </div>
+      </div>
+    )
+  })
+  .add('with text', () => {
+    const name: string = select('Name', names, names[0])
+    return (
+      <div style={{ display: 'flex', fontSize: '2em' }}>
+        <p
+          style={{
+            color: colors.indigo,
+          }}
+        >
+          <Icon name={name} variant="indigo" marginRight="0.25em" />
+          Icon is vertically aligned with text by default
+        </p>
+      </div>
+    )
+  })
