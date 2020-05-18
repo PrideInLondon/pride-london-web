f4207deddfcd2c3c776a143fc3efeb3f783c09c0
schazbot <charlotte.pearce1984@gmail.com>
2020-05-18 14:23:46 +0100

    WEBNEW-125 :tada: video component with cover image that plays video  when clicked

---
+++ src/components/video/Video.styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const VideoContainer = styled.div`
+  position: relative;
+  padding-bottom: 56.25%;
+  margin-bottom: 2em;
+
+  iframe {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+  }
+`
