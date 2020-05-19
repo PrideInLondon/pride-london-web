b8243dcc858b860ec59bef7d599040e8c42592fe
Janine Luk <janine.luk@avast.com>
2020-05-19 23:00:28 +0100

    WEBNEW-137 :lipstick: Create YMUW Cta Button

---
+++ src/components/ymuwButton/YmuwButton.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import { YmuwButton as Button } from './YmuwButton'
+
+describe('<Button/>', () => {
+  it('fires correct method when set onClick property', () => {
+    let counter = 0
+    const handleClick = () => (counter += 1)
+    const wrapper = shallow(<Button onClick={handleClick} />)
+    wrapper.prop('onClick')()
+    expect(counter).toBe(1)
+  })
+
+  it('renders a gatsby link if the to prop is not http', () => {
+    const wrapper = mount(<Button to="/events" />)
+    expect(wrapper.find('GatsbyLink').exists()).toBeTruthy()
+  })
+
+  it('renders a normal a tag if the to prop is https', () => {
+    const wrapper = mount(<Button to="https://google.com" />)
+    expect(wrapper.find('a')).toBeTruthy()
+    expect(wrapper.find('GatsbyLink').exists()).toBeFalsy()
+  })
+
+  it('renders a button tag if there is no to prop', () => {
+    const wrapper = mount(<Button />)
+    expect(wrapper.find('button')).toBeTruthy()
+  })
+})
---
+++ src/components/ymuwButton/index.ts
@@ -0,0 +1,1 @@
+export { YmuwButton } from './YmuwButton'
