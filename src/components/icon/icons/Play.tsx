9d521c5ecdc15a9f82aec7e8d4f8a117a2b1c543
SofiaFredriksson <sofia.fredriksson@seedrs.com>
2020-05-25 11:43:03 +0200

    WEBNEW-132 ✨ Create play button component (#1477)
    * WEBNEW-132 :sparkles: Add indigo box
    
    * WEBNEW-132 :sparkles: create play icon
    
    * WEBNEW-132 :sparkles: add play icon to PlayButton component
    
    * WEBNEW-132 :truck: Rename PlayIcon to Play
    
    * WEBNEW-132 :art: import colors instead of theme
    
    * WEBNEW-132 :ok_hand: fix icon colors
    
    * WEBNEW-132 :ok_hand: fix Icon size and color
    
    * WEBNEW-132 :ok_hand: Change icon center to secondary
    
    * WEBNEW-132 :art: change ButtonWrapper styling and props
    
    * WEBNEW-132 :art: fix props

---
+++ src/components/icon/icons/Play.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { InternalIconProps } from '../Icon.types'
+
+const Play = ({
+  colors: { primary, secondary },
+  ...props
+}: InternalIconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 32 32"
+    {...props}
+  >
+    <path
+      fill={primary}
+      fillRule="evenodd"
+      d="M4 16C4 9.4 9.4 4 16 4s12 5.4 12 12-5.4 12-12 12S4 22.6 4 16zM16 6C10.5 6 6 10.5 6 16s4.5 10 10 10 10-4.5 10-10S21.5 6 16 6z"
+      clipRule="evenodd"
+    />
+    <path
+      fill={secondary}
+      fillRule="evenodd"
+      d="M19 16l-4 3v-6l4 3z"
+      clipRule="evenodd"
+    />
+    <path
+      fill={primary}
+      fillRule="evenodd"
+      d="M22.333 16L13 23V9l9.333 7zM15 13v6l4-3-4-3z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+
+export default Play
