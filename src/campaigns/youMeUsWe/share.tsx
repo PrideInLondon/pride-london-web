4d6f013c16fc1f226b810be548674747c3b518c7
Em McDonald <mcdonald.emily@live.co.uk>
2020-06-02 19:47:23 +0100

    WEBNEW-123 :label: Updated share content type

--- src/campaigns/youMeUsWe/share.tsx
+++ src/campaigns/youMeUsWe/share.tsx
@@ -1,5 +1,9 @@
-export const content = {
+import { ShareContent } from '../../utils/share-utils'
+
+const URL = 'https://prideinlondon.org/ymuw'
+
+export const content: ShareContent = {
   title: 'You! Me! Us! We!',
-  body:
-    'Celebrating allyship and standing up for one another #YouMeUsWe is a rallying cry for Pride 2020 https://prideinlondon.org/ymuw #PrideInLondon',
+  body: `Celebrating allyship and standing up for one another #YouMeUsWe is a rallying cry for Pride 2020 ${URL} #PrideInLondon`,
+  url: URL,
 }
