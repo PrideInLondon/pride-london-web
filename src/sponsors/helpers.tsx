a781f3dd085713b589bc2cf6d422bf0eedee2908
Em McDonald <mcdonald.emily@live.co.uk>
2020-06-18 19:12:12 +0100

    WEBNEW-168 :label: Converted helpers to ts

--- src/sponsors/helpers.tsx
+++ src/sponsors/helpers.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import SponsorBadge from './SponsorBadge'
+import { Sponsor } from './PartnersPage.types'
 
-export const renderSponsors = (sponsors = [], preventLevelSize = false) =>
+export const renderSponsors = (
+  sponsors: Sponsor[] = [],
+  preventLevelSize = false
+) =>
   sponsors.map(sponsor => (
     <SponsorBadge
       key={sponsor.name}
