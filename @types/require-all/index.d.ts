eb2757d79c1f5b556588db5a42f6759ca8f1cd61
egmcdonald <mcdonald.emily@live.co.uk>
2020-06-22 17:20:31 +0100

    WEBREF-110 :heavy_minus_sign: Remove require-all dep

--- @types/require-all/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-declare module 'require-all' {
-  type requireAllDefault = () => { [key: string]: React.ReactNode }
-
-  declare const requireAll: requireAllDefault
-  // eslint-disable-next-line import/no-default-export
-  export default requireAll
-}
