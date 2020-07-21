074532bfa0c149c0bcd18872e23c7a7233ef4747
Em McDonald <mcdonald.emily@live.co.uk>
2020-07-21 17:39:13 +0100

    WEBNEW-190 :lipstick: Extended gallery container to take space props

--- src/components/gallery/GalleryContainer.types.ts
+++ src/components/gallery/GalleryContainer.types.ts
@@ -1,8 +1,10 @@
+import { SpaceProps } from 'styled-system'
+
 export const GALLERY_CONTAINER_VARIANTS = ['grid', 'masonry'] as const
 
 type GalleryContainerVariant = typeof GALLERY_CONTAINER_VARIANTS[number]
 
-export interface GalleryContainerProps {
+export interface GalleryContainerProps extends SpaceProps {
   columns: { [key: string]: number }
   variant: GalleryContainerVariant
 }
--- src/components/masonry/Masonry.styles.ts
+++ src/components/masonry/Masonry.styles.ts
@@ -1,10 +1,13 @@
 import styled from 'styled-components'
+import { space } from 'styled-system'
 import { sm } from '../../theme/space'
 
 export const Wrapper = styled.div`
   display: grid;
   grid-auto-flow: column;
   grid-gap: ${sm}px;
+
+  ${space}
 `
 
 export const Column = styled.div`
