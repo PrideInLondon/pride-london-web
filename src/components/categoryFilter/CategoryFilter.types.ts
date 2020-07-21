c3691b3df2a1503198ba5ad0e39af45f98135607
Em McDonald <mcdonald.emily@live.co.uk>
2020-07-21 16:32:49 +0100

    WEBNEW-190 :art: Simplified category filter props

--- src/components/categoryFilter/CategoryFilter.types.ts
+++ src/components/categoryFilter/CategoryFilter.types.ts
@@ -3,13 +3,13 @@ const FILTER_TYPES = ['checkbox', 'radio'] as const
 export type FilterType = typeof FILTER_TYPES[number]
 
 export interface Category {
-  title: string
-  hexColour: string
+  name: string
+  color: string
 }
 
 export interface CategoryFilterProps {
   filterType: FilterType
   categories: Category[]
   selected: string | string[]
-  handleFilterSelect: (selectedFilterTitle: string) => void
+  handleFilterSelect: (selectedFilterName: string) => void
 }
