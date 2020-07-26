aff68297e5ee6a9c42fbe5970ac18361e0782d6e
Em McDonald <mcdonald.emily@live.co.uk>
2020-07-26 13:42:13 +0100

    WEBNEW-182 :sparkles: Pulled artist bio from cms

--- src/fifty-two/entry/AboutTheArtist.tsx
+++ src/fifty-two/entry/AboutTheArtist.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'gatsby' // lgtm [js/unused-local-variable]
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { lg, xl_mob } from '../../theme/space'
-import { H6, P } from '../../components/typography'
+import { H6 } from '../../components/typography'
 import { Icon } from '../../components/icon'
 import { handleUrl } from '../../utils/location-utils'
+import { renderMethods } from '../renderMethods'
 import { Wrapper, ShareBar, StyledLink } from './AboutTheArtist.styles'
 import { Artist } from './FiftyTwoEntryPage.types'
 import { SocialLinkProps } from './AboutTheArtist.types'
@@ -15,6 +17,7 @@ const SocialLink: React.FC<SocialLinkProps> = ({ url, name }) => (
 )
 
 export const AboutTheArtist: React.FC<Artist> = ({
+  bio: { json },
   website,
   email,
   facebook,
@@ -23,13 +26,7 @@ export const AboutTheArtist: React.FC<Artist> = ({
 }) => (
   <Wrapper padding={`${xl_mob}px ${lg}px`} marginTop="xxl">
     <H6>About the artist</H6>
-    <P paddingBottom="lg">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in
-      culpa qui officia deserunt mollit anim id est laborum.
-    </P>
+    {documentToReactComponents(json, renderMethods)}
     <ShareBar>
       {website && <SocialLink url={website} name="website" />}
       {email && <SocialLink url={`mailto:${email}`} name="email" />}
