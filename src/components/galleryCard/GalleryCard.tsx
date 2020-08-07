3233cd0f6a00253e0a12faef3d36210e6f2a8fbf
schazbot <charlotte.pearce1984@gmail.com>
2020-08-07 16:58:42 +0100

    WEBNEW-195 :bug: adjusted spacing between artist name date and price and changed latter spacing of h3

--- src/components/galleryCard/GalleryCard.tsx
+++ src/components/galleryCard/GalleryCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CardTitle, CardContent, CardFooter } from '../card'
 import { H5, H6, P } from '../typography'
-import { md } from '../../theme/space'
+import { sm, md, lg } from '../../theme/space'
 import {
   StyledCard,
   ImageWrapper,
@@ -28,9 +28,11 @@ export const GalleryCard = ({
     </ImageWrapper>
     <CardContent position="relative" paddingTop={`${md * 2}px`}>
       <StyledTag color={category.color}>{category.name}</StyledTag>
-      <H5 as={CardTitle}>{title}</H5>
-      <P>{name}</P>
-      <P>{year}</P>
+      <H5 letterSpacing="-0.5px" as={CardTitle}>
+        {title}
+      </H5>
+      <P marginBottom={`${sm}px`}>{name}</P>
+      <P marginBottom={`${lg}px`}>{year}</P>
       <H6 as={CardFooter}>{sold ? 'Sold' : guidePrice}</H6>
     </CardContent>
   </StyledCard>
