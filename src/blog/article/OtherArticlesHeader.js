2681f03a571b51e671f4cd4831ce57b6f9d427d1
SofiaFredriksson <sofiapkf@gmail.com>
2020-08-14 12:12:06 +0200

    WEBNEW-60 :ok_hand: fix gatsby-link styled component

--- src/blog/article/OtherArticlesHeader.js
+++ src/blog/article/OtherArticlesHeader.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import Link from 'gatsby-link'
 
-import { Title, Header, StyledChevronIcon } from './OtherArticlesHeader.styles'
+import {
+  StyledLink,
+  Title,
+  Header,
+  StyledChevronIcon,
+} from './OtherArticlesHeader.styles'
 
 const OtherArticlesHeader = () => (
   <Header>
     <Title>You may also like</Title>
-    <Link href="/news-and-views">
+    <StyledLink to="/news-and-views">
       View all articles <StyledChevronIcon />
-    </Link>
+    </StyledLink>
   </Header>
 )
 
