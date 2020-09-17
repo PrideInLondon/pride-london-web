479b942b977b153960158dcbc70e030e9be097d5
Em McDonald <mcdonald.emily@live.co.uk>
2020-09-17 20:31:16 +0100

    WEBREF-116 :wrench: Explicitly define a base path

--- .storybook/config.js
+++ .storybook/config.js
@@ -22,6 +22,11 @@ global.___loader = {
 }
 // Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
 global.__PATH_PREFIX__ = ''
+
+// This global variable is prevents the "__BASE_PATH__ is not defined" error inside Storybook.
+// Required for gatsby-link: https://github.com/gatsbyjs/gatsby/issues/10668#issuecomment-639014099
+global.__BASE_PATH__ = ''
+
 // This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
 window.___navigate = pathname => {
   action('NavigateTo:')(pathname)
