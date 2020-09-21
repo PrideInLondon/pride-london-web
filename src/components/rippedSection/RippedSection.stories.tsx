b64e8c477dde938a4550a538be56af388d131da1
Em McDonald <mcdonald.emily@live.co.uk>
2020-09-21 19:22:25 +0100

    WEBNEW-155 :sparkles: Added story for rip with image

--- src/components/rippedSection/RippedSection.stories.tsx
+++ src/components/rippedSection/RippedSection.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { select, color } from '@storybook/addon-knobs'
+import { select, color, files } from '@storybook/addon-knobs'
 import { colors } from '../../theme/colors'
+import logo from '../../assets/logo.png'
 import { RippedSection } from './RippedSection'
 import { RipVariant } from './Rip.types'
 
@@ -52,5 +53,31 @@ storiesOf(RippedSection.name, module).add('Bottom variant', () => (
     }}
   >
     <div style={{ height: 400 }} />
+  </RippedSection>
+))
+
+storiesOf(RippedSection.name, module).add('With background image', () => (
+  <RippedSection
+    rips={{
+      top: {
+        variant: select('Top Rip Variant', variants, variants[0], 'Top Rip'),
+        color: color('Top Rip Color', colors.white, 'Top Rip'),
+      },
+      bottom: {
+        variant: select(
+          'Bottom Rip Variant',
+          variants,
+          variants[0],
+          'Bottom Rip'
+        ),
+        color: color('Bottom Rip Color', colors.white, 'Bottom Rip'),
+      },
+    }}
+    maxWidth={550}
+  >
+    <img
+      src={files('fixed', '.jpg, .png', [logo], 'image')[0]}
+      alt="Example for Storybook"
+    />
   </RippedSection>
 ))
