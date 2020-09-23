25e0f0f46c267aef56d17e7dc3e460bea420e1c4
Em McDonald <mcdonald.emily@live.co.uk>
2020-09-23 21:01:30 +0100

    WEBREF-121 :recycle: Convert FilterContainer stories to CSF

--- src/components/filterContainer/FilterContainer.stories.tsx
+++ src/components/filterContainer/FilterContainer.stories.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 import { object, optionsKnob as options } from '@storybook/addon-knobs'
 import { colors } from '../../theme/colors'
 import FilterContainer from './FilterContainer'
@@ -17,29 +16,41 @@ const CATEGORY_OPTIONS: { [key: string]: string } = CATEGORIES.reduce(
   {}
 )
 
-storiesOf(FilterContainer.name, module)
-  .add('checkbox', () => (
-    <FilterContainer
-      filterType="checkbox"
-      categories={object('categories', CATEGORIES)}
-      selected={options(
-        'selected',
-        CATEGORY_OPTIONS,
-        [CATEGORY_OPTIONS[CATEGORIES[0].title]],
-        {
-          display: 'inline-check',
-        }
-      )}
-      handleFilterSelect={() => {}}
-    />
-  ))
-  .add('radio', () => (
-    <FilterContainer
-      filterType="radio"
-      categories={object('categories', CATEGORIES)}
-      selected={options('selected', CATEGORY_OPTIONS, CATEGORIES[0].title, {
-        display: 'inline-radio',
-      })}
-      handleFilterSelect={() => {}}
-    />
-  ))
+export default {
+  title: 'FilterContainer',
+}
+
+export const Checkbox = () => (
+  <FilterContainer
+    filterType="checkbox"
+    categories={object('categories', CATEGORIES)}
+    selected={options(
+      'selected',
+      CATEGORY_OPTIONS,
+      [CATEGORY_OPTIONS[CATEGORIES[0].title]],
+      {
+        display: 'inline-check',
+      }
+    )}
+    handleFilterSelect={() => {}}
+  />
+)
+
+Checkbox.story = {
+  name: 'checkbox',
+}
+
+export const Radio = () => (
+  <FilterContainer
+    filterType="radio"
+    categories={object('categories', CATEGORIES)}
+    selected={options('selected', CATEGORY_OPTIONS, CATEGORIES[0].title, {
+      display: 'inline-radio',
+    })}
+    handleFilterSelect={() => {}}
+  />
+)
+
+Radio.story = {
+  name: 'radio',
+}
