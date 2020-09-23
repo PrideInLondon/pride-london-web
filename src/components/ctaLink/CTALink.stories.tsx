7e76c12c96887a5ae0179fc472afd2f131e95f97
Em McDonald <mcdonald.emily@live.co.uk>
2020-09-23 20:58:12 +0100

    WEBREF-121 :recycle: Convert CTALink stories to CSF

--- src/components/ctaLink/CTALink.stories.tsx
+++ src/components/ctaLink/CTALink.stories.tsx
@@ -1,16 +1,17 @@
-import { storiesOf } from '@storybook/react'
 import React from 'react'
-import { text } from '@storybook/addon-knobs'
+import { text, boolean } from '@storybook/addon-knobs'
 import { CTALink } from './CTALink'
 
-storiesOf('CTALink', module)
-  .add('with URL', () => (
-    <CTALink to={text('Link URL', '/')}>
-      {text('Link Text', 'This is a CTA Link')}
-    </CTALink>
-  ))
-  .add('without arrow', () => (
-    <CTALink to={text('Link URL', '/')} arrow={false}>
-      {text('Link Text', 'This is a CTA Link')}
-    </CTALink>
-  ))
+export default {
+  title: 'CTALink',
+}
+
+export const Default = () => (
+  <CTALink to={text('Link URL', '/')} arrow={boolean('arrow', true)}>
+    {text('Link Text', 'This is a CTA Link')}
+  </CTALink>
+)
+
+Default.story = {
+  name: 'default',
+}
