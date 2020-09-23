000ea7c2648a45b75c38e18371beb8f5f1bd47b4
Em McDonald <mcdonald.emily@live.co.uk>
2020-09-23 20:59:10 +0100

    WEBREF-121 :recycle: Convert Divider stories to CSF

--- src/components/divider/Divider.stories.tsx
+++ src/components/divider/Divider.stories.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 import { colors } from '../../theme/colors'
 import { md } from '../../theme/space'
 import { Divider } from './Divider'
 
-storiesOf('Divider', module).add('default', () => (
+export default {
+  title: 'Divider',
+}
+
+export const Default = () => (
   <div style={{ backgroundColor: colors.white, padding: md }}>
     <Divider marginY="xl" />
   </div>
-))
+)
+
+Default.story = {
+  name: 'default',
+}
