28bb49000387adeab5b54741205cca9ea49b0f7a
egmcdonald <mcdonald.emily@live.co.uk>
2020-09-24 14:33:14 +0100

    WEBREF-121 :label: Fixed type error

--- src/components/video/Video.types.ts
+++ src/components/video/Video.types.ts
@@ -2,7 +2,7 @@ import { LayoutProps, SpaceProps } from 'styled-system'
 
 export const VIDEO_HOSTS = ['vimeo', 'youtube'] as const
 
-type VideoHost = typeof VIDEO_HOSTS[number]
+export type VideoHost = typeof VIDEO_HOSTS[number]
 
 export interface VideoProps extends LayoutProps, SpaceProps {
   host: VideoHost
