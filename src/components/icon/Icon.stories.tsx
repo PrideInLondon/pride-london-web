24b9f3c2189c8c32b0a624118cfa927dedc6cdc4
egmcdonald <mcdonald.emily@live.co.uk>
2020-09-24 12:08:54 +0100

    WEBREF-121 :recycle: Convert Icon stories to CSF

--- src/components/icon/Icon.stories.tsx
+++ src/components/icon/Icon.stories.tsx
@@ -1,58 +1,61 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 import { select } from '@storybook/addon-knobs'
 import { colors } from '../../theme/colors'
 import { Icon } from './Icon'
 import { NAMES } from './icons'
 import { ROTATIONS } from './Icon.utils'
 
-storiesOf('Icon', module)
-  .add('variant', () => {
-    const name = select('Name', NAMES, NAMES[0])
-    return (
-      <div style={{ display: 'flex', fontSize: '2em' }}>
-        <div
-          style={{
-            backgroundColor: colors.white,
-            color: colors.indigo,
-            padding: '1em',
-          }}
-        >
-          <Icon name={name} variant="indigo" />
-        </div>
-        <div
-          style={{
-            backgroundColor: colors.indigo,
-            color: colors.white,
-            padding: '1em',
-          }}
-        >
-          <Icon name={name} variant="white" />
-        </div>
+export default {
+  title: 'Icon',
+}
+
+export const Default = () => {
+  const name = select('Name', NAMES, NAMES[0])
+  const rotate = select('Rotate', ROTATIONS, ROTATIONS[0])
+  return (
+    <div style={{ display: 'flex', fontSize: '2em' }}>
+      <div
+        style={{
+          backgroundColor: colors.white,
+          color: colors.indigo,
+          padding: '1em',
+        }}
+      >
+        <Icon name={name} variant="indigo" rotate={rotate} />
       </div>
-    )
-  })
-  .add('rotate', () => (
-    <div style={{ fontSize: '2em' }}>
-      <Icon
-        name={select('Name', NAMES, NAMES[0])}
-        variant="indigo"
-        rotate={select('Rotate', ROTATIONS, ROTATIONS[0])}
-      />
+      <div
+        style={{
+          backgroundColor: colors.indigo,
+          color: colors.white,
+          padding: '1em',
+        }}
+      >
+        <Icon name={name} variant="white" rotate={rotate} />
+      </div>
     </div>
-  ))
-  .add('with text', () => {
-    const name = select('Name', NAMES, NAMES[0])
-    return (
-      <div style={{ fontSize: '2em' }}>
-        <p
-          style={{
-            color: colors.indigo,
-          }}
-        >
-          <Icon name={name} variant="indigo" marginRight="0.25em" />
-          Icon is vertically aligned with text by default
-        </p>
-      </div>
-    )
-  })
+  )
+}
+
+Default.story = {
+  name: 'default',
+}
+
+export const WithText = () => {
+  const name = select('Name', NAMES, NAMES[0])
+  return (
+    <div style={{ fontSize: '2em' }}>
+      <p
+        style={{
+          color: colors.indigo,
+        }}
+      >
+        <Icon name={name} variant="indigo" marginRight="0.25em" />
+        Icon is vertically aligned with text by default
+      </p>
+    </div>
+  )
+}
+
+WithText.story = {
+  name: 'with text',
+}
