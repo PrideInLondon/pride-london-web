9055561a5b1c13e2ede745c513aedfcfb28f3198
egmcdonald <mcdonald.emily@live.co.uk>
2020-09-24 12:47:27 +0100

    WEBREF-121 :recycle: Convert Typeform stories to CSF

--- src/components/typeform/Typeform.stories.tsx
+++ src/components/typeform/Typeform.stories.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
 import { Button } from '../button'
 import { Typeform } from './Typeform'
 
-storiesOf(Typeform.name, module).add('with Button', () => (
+export default {
+  title: 'Typeform',
+}
+
+export const WithButton = () => (
   <Typeform id={text('Typeform ID', 'RLnK9L')}>
     <Button>Click me</Button>
   </Typeform>
-))
+)
+
+WithButton.story = {
+  name: 'with Button',
+}
