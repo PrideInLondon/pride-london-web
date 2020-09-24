2b7bd6bcd111d6d992a263a7b209bf4d1562df03
egmcdonald <mcdonald.emily@live.co.uk>
2020-09-24 12:46:35 +0100

    WEBREF-121 :recycle: Convert Tag stories to CSF

--- src/components/tag/Tag.stories.tsx
+++ src/components/tag/Tag.stories.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 import { select, text, color } from '@storybook/addon-knobs'
 import { colors } from '../../theme/colors'
 import { Tag } from './Tag'
+import { TAG_VARIANTS } from './Tag.types'
 
-const variants = {
-  Primary: 'primary',
-  Outline: 'outline',
-} as const
+export default {
+  title: 'Tag',
+}
 
-storiesOf('Tag', module).add('default', () => (
+export const Default = () => (
   <Tag
     color={color('Color', colors.indigo)}
-    variant={select('Variant', variants, variants.Primary)}
+    variant={select('Variant', TAG_VARIANTS, TAG_VARIANTS[0])}
   >
     {text('Text', 'Pride in London')}
   </Tag>
-))
+)
+
+Default.story = {
+  name: 'default',
+}
--- src/components/tag/Tag.types.ts
+++ src/components/tag/Tag.types.ts
@@ -1,8 +1,12 @@
 import { SpaceProps } from 'styled-system'
 
+export const TAG_VARIANTS = ['primary', 'outline'] as const
+
+type TagVariant = typeof TAG_VARIANTS[number]
+
 export interface TagProps
   extends React.HTMLAttributes<HTMLDivElement>,
     SpaceProps {
   color: string
-  variant?: 'primary' | 'outline'
+  variant?: TagVariant
 }
