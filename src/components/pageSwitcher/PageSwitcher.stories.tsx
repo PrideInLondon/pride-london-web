4b12100b03a1f5665def67274d2064fe8a733b61
egmcdonald <mcdonald.emily@live.co.uk>
2020-09-24 12:31:27 +0100

    WEBREF-121 :recycle: Convert PageSwitcher stories to CSF

--- src/components/pageSwitcher/PageSwitcher.stories.tsx
+++ src/components/pageSwitcher/PageSwitcher.stories.tsx
@@ -1,25 +1,30 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
 import { PageSwitcher } from './PageSwitcher'
 
-storiesOf('PageSwitcher', module).add('default', () => {
-  return (
-    <PageSwitcher
-      prev={{
-        title: text('Previous text', 'Previous artwork title'),
-        subtitle: text('Previous subtitle', 'Previous artist name'),
-        url: text('Previous url', '/previous'),
-      }}
-      next={{
-        title: text('Next text', 'Next artwork title'),
-        subtitle: text('Next subtitle', 'Next artist name'),
-        url: text('Next url', '/next'),
-      }}
-      back={{
-        title: text('Back text', 'Back to gallery'),
-        url: text('Back url', '/'),
-      }}
-    />
-  )
-})
+export default {
+  title: 'PageSwitcher',
+}
+
+export const Default = () => (
+  <PageSwitcher
+    prev={{
+      title: text('Previous text', 'Previous artwork title', 'Previous'),
+      subtitle: text('Previous subtitle', 'Previous artist name', 'Previous'),
+      url: text('Previous url', '/previous', 'Previous'),
+    }}
+    next={{
+      title: text('Next text', 'Next artwork title', 'Next'),
+      subtitle: text('Next subtitle', 'Next artist name', 'Next'),
+      url: text('Next url', '/next', 'Next'),
+    }}
+    back={{
+      title: text('Back text', 'Back to gallery'),
+      url: text('Back url', '/'),
+    }}
+  />
+)
+
+Default.story = {
+  name: 'default',
+}
