063d40ab82c5b779d6d9edf66f2f48cafebc63db
egmcdonald <mcdonald.emily@live.co.uk>
2020-09-25 18:52:11 +0100

    WEBREF-121 :lipstick: Added theme to SB

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,20 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import theme from '../src/theme/theme'
+import { GlobalStyle } from '../src/layout/GlobalStyle'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 }
+
+export const decorators = [
+  story => (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        {story()}
+      </>
+    </ThemeProvider>
+  ),
+]
