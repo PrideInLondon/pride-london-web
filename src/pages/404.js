d154be31ddba132071d071d2d64c77ce2bf899c6
Em McDonald <mcdonald.emily@live.co.uk>
2020-10-21 22:44:44 +0100

    WEBNEW-200 :chart_with_upwards_trend: Added custom title for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { Helmet } from '../components/helmet'
 import { PageIntro } from '../components/pageIntro'
 import { H3, P } from '../components/typography'
 
 const NotFoundPage = () => (
-  <PageIntro>
-    <H3 as="h1">Page Not Found</H3>
-    <P>You just hit a route that doesn&#39;t exist... the sadness.</P>
-  </PageIntro>
+  <>
+    <Helmet title="Page Not Found" />
+    <PageIntro>
+      <H3 as="h1">Page Not Found</H3>
+      <P>You just hit a route that doesn&#39;t exist... the sadness.</P>
+    </PageIntro>
+  </>
 )
 
 export default NotFoundPage
