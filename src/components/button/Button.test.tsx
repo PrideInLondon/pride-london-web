c2e6e1282befd483825cea7e8967f400c6e319b0
schazbot <charlotte.pearce1984@gmail.com>
2020-10-23 18:40:05 +0100

    WEBREF-81 :recycle: Removed falsy statements and added fireEvent to Button

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -1,30 +1,26 @@
 import React from 'react'
 import { axe } from 'jest-axe'
-import { render } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render, fireEvent } from '@testing-library/react'
 import { Button } from './Button'
 
 describe('Button', () => {
   it('shoud render a button if there is no to prop', () => {
     const { queryByRole } = render(<Button />)
     expect(queryByRole('button')).toBeTruthy()
-    expect(queryByRole('link')).toBeFalsy()
   })
 
   it('renders a normal a tag if the to prop is https', () => {
     const { queryByRole } = render(<Button to="https://google.com" />)
     expect(queryByRole('link')).toBeTruthy()
-    expect(queryByRole('button')).toBeFalsy()
   })
 
   it('should fire function when given an onClick property', () => {
-    let counter = 0
-    const handleClick = () => (counter += 1)
+    const handleClick = jest.fn()
     const { getByText } = render(
       <Button onClick={handleClick}>Add One More</Button>
     )
-    userEvent.click(getByText('Add One More'))
-    expect(counter).toBe(1)
+    fireEvent.click(getByText('Add One More'))
+    expect(handleClick).toHaveBeenCalledTimes(1)
   })
 
   it('should have no accessibility violations', async () => {
