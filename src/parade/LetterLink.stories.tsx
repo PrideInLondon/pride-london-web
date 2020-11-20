0f6728b64a24b2ba8cb8dc84ba5d2b124e79871b
schazbot <charlotte.pearce1984@gmail.com>
2020-11-20 12:38:50 +0000

    WEBREF-76 :recycle: Changes as per review

--- src/parade/LetterLink.stories.tsx
+++ src/parade/LetterLink.stories.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { text, boolean } from '@storybook/addon-knobs'
 import LetterLink from './LetterLink'
 
 storiesOf('LetterLink', module).add('default', () => (
-  <LetterLink letter="a" isActive />
+  <LetterLink
+    isActive={boolean('isActive', false)}
+    letter={text('Letter text', 'a')}
+  />
 ))
