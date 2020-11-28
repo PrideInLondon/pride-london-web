23ebba57ede0f273ce1109f260ec9b7bcdd14ce4
schazbot <charlotte.pearce1984@gmail.com>
2020-11-28 12:12:01 +0000

    WEBREF-76 :fire: Remove last test related to styling

--- src/parade/LetterLink.test.tsx
+++ src/parade/LetterLink.test.tsx
@@ -4,11 +4,6 @@ import { render } from '@testing-library/react'
 import LetterLink from './LetterLink'
 
 describe('LetterLink', () => {
-  it('should render disabled StyledLetterLink with the correct styles', () => {
-    const { queryByText } = render(<LetterLink letter="a" isDisabled />)
-    expect(queryByText(/a/i)).toHaveStyle('color: grey')
-  })
-
   it('should have no accessibility violations', async () => {
     const { container } = render(<LetterLink letter="a" isActive />)
     const results = await axe(container)
