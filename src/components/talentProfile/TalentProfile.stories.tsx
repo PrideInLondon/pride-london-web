71ed20e718ab520c111ef28428918925bb514fc9
Em McDonald <mcdonald.emily@live.co.uk>
2020-12-03 20:15:23 +0000

    WEBNEW-211 :lipstick: Updated story to use knobs

--- src/components/talentProfile/TalentProfile.stories.tsx
+++ src/components/talentProfile/TalentProfile.stories.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { text } from '@storybook/addon-knobs'
 import { TalentProfile } from './'
 
 storiesOf('TalentProfile', module).add('default', () => (
   <TalentProfile
-    name="Lorem ipsum"
-    website="a"
-    email="b"
-    facebook="c"
-    twitter="d"
-    instagram="e"
+    name={text('name', 'Lorem ipsum')}
+    website={text('website', 'https://prideinlondon.org')}
+    email={text('email', 'example@prideinlondon.org')}
+    facebook={text('facebook', 'https://facebook.com/PrideinLondon')}
+    twitter={text('twitter', 'https://twitter.com/PrideInLondon')}
+    instagram={text('instagram', 'https://instagram.com/prideinlondon')}
   />
 ))
