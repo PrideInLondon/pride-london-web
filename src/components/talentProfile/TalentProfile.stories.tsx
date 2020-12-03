520d5f56430129f802809d02a0e1d4a13f0d0a15
Em McDonald <mcdonald.emily@live.co.uk>
2020-12-03 23:24:32 +0000

    WEBNEW-211 :lipstick: Removed extra wrapping div

--- src/components/talentProfile/TalentProfile.stories.tsx
+++ src/components/talentProfile/TalentProfile.stories.tsx
@@ -4,12 +4,15 @@ import { text } from '@storybook/addon-knobs'
 import { TalentProfile } from './'
 
 storiesOf('TalentProfile', module).add('default', () => (
-  <TalentProfile
-    type={text('type', 'author')}
-    website={text('website', 'https://prideinlondon.org')}
-    email={text('email', 'example@prideinlondon.org')}
-    facebook={text('facebook', 'https://facebook.com/PrideinLondon')}
-    twitter={text('twitter', 'https://twitter.com/PrideInLondon')}
-    instagram={text('instagram', 'https://instagram.com/prideinlondon')}
-  />
+  <div style={{ backgroundColor: 'white', padding: 20 }}>
+    <TalentProfile
+      type={text('type', 'author')}
+      website={text('website', 'https://prideinlondon.org')}
+      email={text('email', 'example@prideinlondon.org')}
+      facebook={text('facebook', 'https://facebook.com/PrideinLondon')}
+      twitter={text('twitter', 'https://twitter.com/PrideInLondon')}
+      instagram={text('instagram', 'https://instagram.com/prideinlondon')}
+      maxWidth={600}
+    />
+  </div>
 ))
--- src/components/talentProfile/TalentProfile.tsx
+++ src/components/talentProfile/TalentProfile.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Link } from 'gatsby' // lgtm [js/unused-local-variable]
-import { lg, xl_mob } from '../../theme/space'
 import { H6 } from '../../components/typography'
 import { Icon } from '../../components/icon'
 import { handleUrl } from '../../utils/location-utils'
-import { Wrapper, Content, ShareBar, StyledLink } from './TalentProfile.styles'
+import { Wrapper, ShareBar, StyledLink } from './TalentProfile.styles'
 import { SocialLinkProps, TalentProfileProps } from './TalentProfile.types'
 
 const SocialLink: React.FC<SocialLinkProps> = ({ url, name }) => (
@@ -23,15 +22,13 @@ export const TalentProfile: React.FC<TalentProfileProps> = ({
   ...props
 }) => (
   <Wrapper {...props}>
-    <Content padding={`${xl_mob}px ${lg}px`} marginTop="xxl">
-      <H6 as="h3">About the {type}</H6>
-      <ShareBar>
-        {website && <SocialLink url={website} name="website" />}
-        {email && <SocialLink url={`mailto:${email}`} name="email" />}
-        {facebook && <SocialLink url={facebook} name="facebook" />}
-        {twitter && <SocialLink url={twitter} name="twitter" />}
-        {instagram && <SocialLink url={instagram} name="instagram" />}
-      </ShareBar>
-    </Content>
+    <H6 as="h3">About the {type}</H6>
+    <ShareBar>
+      {website && <SocialLink url={website} name="website" />}
+      {email && <SocialLink url={`mailto:${email}`} name="email" />}
+      {facebook && <SocialLink url={facebook} name="facebook" />}
+      {twitter && <SocialLink url={twitter} name="twitter" />}
+      {instagram && <SocialLink url={instagram} name="instagram" />}
+    </ShareBar>
   </Wrapper>
 )
