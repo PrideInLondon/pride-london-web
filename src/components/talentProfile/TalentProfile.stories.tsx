0271a362aa96dfc1f19df7a917aedd6ea8bc7b8a
Em McDonald <mcdonald.emily@live.co.uk>
2020-12-04 00:15:40 +0000

    WEBNEW-211 :white_check_mark: Added test for a11y violations

--- src/components/talentProfile/TalentProfile.stories.tsx
+++ src/components/talentProfile/TalentProfile.stories.tsx
@@ -1,65 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
-import { Document } from '@contentful/rich-text-types'
+import { json } from './__mocks__'
 import { TalentProfile } from './'
-
-// nodeType properties below ignored due to issue with Contentful types
-// see https://github.com/contentful/rich-text/issues/123
-const json: Document = {
-  content: [
-    {
-      data: {},
-      content: [
-        {
-          data: {},
-          marks: [],
-          value:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea ',
-          nodeType: 'text',
-        },
-        {
-          data: { uri: 'https://prideinlondon.org' },
-          content: [
-            {
-              data: {},
-              marks: [],
-              value: 'commodo consequat',
-              nodeType: 'text',
-            },
-          ],
-          // @ts-ignore
-          nodeType: 'hyperlink',
-        },
-        {
-          data: {},
-          marks: [],
-          value: '.',
-          // @ts-ignore
-          nodeType: 'text',
-        },
-      ],
-      // @ts-ignore
-      nodeType: 'paragraph',
-    },
-    {
-      data: {},
-      content: [
-        {
-          data: {},
-          marks: [],
-          value:
-            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          nodeType: 'text',
-        },
-      ],
-      // @ts-ignore
-      nodeType: 'paragraph',
-    },
-  ],
-  // @ts-ignore
-  nodeType: 'document',
-}
 
 storiesOf('TalentProfile', module).add('default', () => (
   <div style={{ backgroundColor: 'white', padding: 20 }}>
