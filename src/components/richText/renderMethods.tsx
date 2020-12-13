df72e9b2f5d047e00ff11335ddfa2d0bc1b664b3
Em McDonald <mcdonald.emily@live.co.uk>
2020-12-13 19:28:57 +0000

    WEBNEW-213 :recycle: Updated hyperlink render to use Navigate

--- src/components/richText/renderMethods.tsx
+++ src/components/richText/renderMethods.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 import { NodeRenderer } from '@contentful/rich-text-react-renderer'
 import { BLOCKS, INLINES } from '@contentful/rich-text-types'
 import styled from 'styled-components'
-
 import { colors } from '../../theme/colors'
 import { P } from '../typography'
+import { Navigate } from '../navigate'
 
 const renderParagraph: NodeRenderer = (_node, children) => <P>{children}</P>
 
-const BodyLink = styled.a`
+const BodyLink = styled(Navigate)`
   font-weight: 500;
   color: ${colors.indigo};
 `
 
 const renderHyperlink: NodeRenderer = ({ data: { uri } }, children) => (
-  <BodyLink href={uri}>{children}</BodyLink>
+  <BodyLink to={uri}>{children}</BodyLink>
 )
 
 export const renderMethods = {
