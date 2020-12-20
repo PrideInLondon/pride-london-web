030f3fe2b6c5a1e4d57fc94bf982f02b0e390624
egmcdonald <mcdonald.emily@live.co.uk>
2020-12-20 18:19:54 +0000

    WEBNEW-213 :lipstick: Added placeholder image component

---
+++ src/components/image/Image.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { ImageProps } from './Image.types'
+
+// eslint-disable-next-line jsx-a11y/alt-text
+export const Image: React.FC<ImageProps> = props => <img {...props} />
