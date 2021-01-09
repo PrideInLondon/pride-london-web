bcf0ffbac68358a1eac157b5b34076f6273cecd7
Em McDonald <mcdonald.emily@live.co.uk>
2021-01-09 20:14:23 +0000

    WEBNEW-214 :sparkles: Added hero image to blog article page

--- src/blog/article/BlogArticlePage.tsx
+++ src/blog/article/BlogArticlePage.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Image from 'gatsby-image/withIEPolyfill'
+import { RippedSection } from '../../components/rippedSection'
+import { RipVariant } from '../../components/rippedSection/Rip.types'
+import { getImageForBreakpoint } from '../../utils/style-utils'
+import { getRandomInt } from '../../utils/number-utils'
+import { colors } from '../../theme/colors'
 import { BlogArticlePageProps } from './BlogArticlePage.types'
 
 export const BlogArticlePage: React.FC<BlogArticlePageProps> = ({
-  data: { contentfulBlogArticle: article },
-}) => {
-  console.log('article: ', article)
-  return <h1>blah</h1>
-}
+  data: {
+    contentfulBlogArticle: { hero },
+  },
+}) => (
+  <>
+    <RippedSection
+      rips={{
+        bottom: {
+          color: colors.white,
+          variant: getRandomInt(1, 5) as RipVariant,
+        },
+      }}
+    >
+      <Image
+        fluid={getImageForBreakpoint(hero)}
+        style={{ width: '100%' }}
+        aria-hidden="true"
+      />
+    </RippedSection>
+  </>
+)
