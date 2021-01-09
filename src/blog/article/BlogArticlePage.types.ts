2c0ece5f1d7e11a7b9f9d5fc4749639f830fd306
Em McDonald <mcdonald.emily@live.co.uk>
2021-01-09 20:13:30 +0000

    WEBNEW-214 :alien: Reworked hero content type due to change upstream

--- src/blog/article/BlogArticlePage.types.ts
+++ src/blog/article/BlogArticlePage.types.ts
@@ -1,13 +1,12 @@
 import { Document } from '@contentful/rich-text-types'
+import { FluidObject } from 'gatsby-image'
 import { ContentfulTalentProfile } from '../../components/talentProfile/TalentProfile.types'
 
 interface ContentfulBlogArticle {
   hero: {
-    image: {
-      file: {
-        url: string
-      }
-    }
+    desktop: FluidObject
+    tablet: FluidObject
+    mobile: FluidObject
   }
   category: string
   title: string
