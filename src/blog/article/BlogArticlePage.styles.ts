cb837112fc9f46e89bf180af251dd9fc43bf277a
Em McDonald <mcdonald.emily@live.co.uk>
2021-01-09 20:53:59 +0000

    WEBNEW-214 :lipstick: Added title and catgeory to blog article

---
+++ src/blog/article/BlogArticlePage.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { space, SpaceProps } from 'styled-system'
+
+export const InfoWrapper = styled.div<SpaceProps>`
+  display: flex;
+  justify-content: center;
+
+  ${space}
+`
--- src/blog/article/BlogArticlePage.tsx
+++ src/blog/article/BlogArticlePage.tsx
@@ -2,14 +2,24 @@ import React from 'react'
 import Image from 'gatsby-image/withIEPolyfill'
 import { RippedSection } from '../../components/rippedSection'
 import { RipVariant } from '../../components/rippedSection/Rip.types'
+import { Tag } from '../../components/tag'
+import { H2 } from '../../components/typography'
 import { getImageForBreakpoint } from '../../utils/style-utils'
 import { getRandomInt } from '../../utils/number-utils'
 import { colors } from '../../theme/colors'
+import { InfoWrapper } from './BlogArticlePage.styles'
 import { BlogArticlePageProps } from './BlogArticlePage.types'
+
+const getCategoryColor = (category: string) => {
+  switch (category) {
+    default:
+      return colors.mexicanPink
+  }
+}
 
 export const BlogArticlePage: React.FC<BlogArticlePageProps> = ({
   data: {
-    contentfulBlogArticle: { hero },
+    contentfulBlogArticle: { hero, category, title },
   },
 }) => (
   <>
@@ -27,5 +37,11 @@ export const BlogArticlePage: React.FC<BlogArticlePageProps> = ({
         aria-hidden="true"
       />
     </RippedSection>
+    <InfoWrapper marginTop="xxl" marginBottom="lg">
+      <Tag color={getCategoryColor(category)}>{category}</Tag>
+    </InfoWrapper>
+    <H2 as="h1" textAlign="center">
+      {title}
+    </H2>
   </>
 )
