cfdea16e075e52fa39a643be256931352d703583
Em McDonald <mcdonald.emily@live.co.uk>
2021-01-11 19:48:45 +0000

    WEBNEW-214 :lipstick: Incorporated the rich text component
    Styled out all font-based components.

--- src/blog/article/BlogArticlePage.styles.ts
+++ src/blog/article/BlogArticlePage.styles.ts
@@ -1,9 +1,40 @@
 import styled from 'styled-components'
-import { space, SpaceProps } from 'styled-system'
+import {
+  compose,
+  flexbox,
+  layout,
+  space,
+  FlexboxProps,
+  LayoutProps,
+  SpaceProps,
+} from 'styled-system'
+import { RichText } from '../../components/richText'
+import { fontSizes } from '../../theme/fonts'
+import { mediaQueries } from '../../theme/mediaQueries'
 
-export const InfoWrapper = styled.div<SpaceProps>`
-  display: flex;
-  justify-content: center;
+export const InfoWrapper = styled.div<FlexboxProps & LayoutProps & SpaceProps>`
+  ${compose(flexbox, layout, space)}
+`
 
-  ${space}
+export const TitleWrapper = styled.div<FlexboxProps & LayoutProps & SpaceProps>`
+  ${compose(flexbox, layout, space)}
+`
+
+export const Content = styled(RichText)`
+  ol,
+  ul {
+    width: 100%;
+  }
+
+  > p:first-of-type {
+    font-size: ${fontSizes.body_lg};
+  }
+
+  ${mediaQueries.md} {
+    ol,
+    p,
+    ul {
+      max-width: 624px;
+    }
+  }
 `
--- src/blog/article/BlogArticlePage.tsx
+++ src/blog/article/BlogArticlePage.tsx
@@ -7,8 +7,10 @@ import { H2 } from '../../components/typography'
 import { getImageForBreakpoint } from '../../utils/style-utils'
 import { getRandomInt } from '../../utils/number-utils'
 import { colors } from '../../theme/colors'
-import { InfoWrapper } from './BlogArticlePage.styles'
+import { InfoWrapper, TitleWrapper, Content } from './BlogArticlePage.styles'
 import { BlogArticlePageProps } from './BlogArticlePage.types'
+
+const PADDING_X = { default: 'lg', md: 'xxl' }
 
 const getCategoryColor = (category: string) => {
   switch (category) {
@@ -19,7 +21,12 @@ const getCategoryColor = (category: string) => {
 
 export const BlogArticlePage: React.FC<BlogArticlePageProps> = ({
   data: {
-    contentfulBlogArticle: { hero, category, title },
+    contentfulBlogArticle: {
+      hero,
+      category,
+      title,
+      content: { json },
+    },
   },
 }) => (
   <>
@@ -37,11 +44,25 @@ export const BlogArticlePage: React.FC<BlogArticlePageProps> = ({
         aria-hidden="true"
       />
     </RippedSection>
-    <InfoWrapper marginTop="xxl" marginBottom="lg">
+    <InfoWrapper
+      display="flex"
+      justifyContent="center"
+      marginTop="xxl"
+      marginBottom="lg"
+    >
       <Tag color={getCategoryColor(category)}>{category}</Tag>
     </InfoWrapper>
-    <H2 as="h1" textAlign="center">
-      {title}
-    </H2>
+    <TitleWrapper display="flex" justifyContent="center" paddingX={PADDING_X}>
+      <H2 as="h1" textAlign="center" maxWidth={842}>
+        {title}
+      </H2>
+    </TitleWrapper>
+    <Content
+      document={json}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      paddingX={PADDING_X}
+    />
   </>
 )
