4fdee791f8585b85d0ed505c35a876ecc5550bff
egmcdonald <mcdonald.emily@live.co.uk>
2021-01-20 09:52:12 +0000

    WEBNEW-227 :pencil: Added comment to clarify reasoning behind styled implementation

--- src/components/playButton/PlayButton.styles.tsx
+++ src/components/playButton/PlayButton.styles.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import { compose, border, color, position, space } from 'styled-system'
 import { PlayButtonProps } from './PlayButton.types'
 
+// we need to map this explicitly due to types issue with styled-components
+// this can be moved back to styled.button after v5 upgrade
 export const Button = styled(props => <button {...props} />)<PlayButtonProps>`
   cursor: pointer;
 
