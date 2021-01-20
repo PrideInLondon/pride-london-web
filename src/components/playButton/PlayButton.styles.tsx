fcde90b7255f483aac2d60a67fb7cb5b0dddff5e
egmcdonald <mcdonald.emily@live.co.uk>
2021-01-20 09:49:14 +0000

    WEBNEW-227 :label: Fixed types issue with PlayButton

--- src/components/playButton/PlayButton.styles.tsx
+++ src/components/playButton/PlayButton.styles.tsx
@@ -1,8 +1,9 @@
+import React from 'react'
 import styled from 'styled-components'
 import { compose, border, color, position, space } from 'styled-system'
 import { PlayButtonProps } from './PlayButton.types'
 
-export const Button = styled.button<PlayButtonProps>`
+export const Button = styled(props => <button {...props} />)<PlayButtonProps>`
   cursor: pointer;
 
   ${compose(border, color, position, space)}
