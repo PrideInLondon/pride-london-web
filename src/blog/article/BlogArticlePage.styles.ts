f31012eb1c6ab0df7df25b9c70f02bc79fef8333
Em McDonald <mcdonald.emily@live.co.uk>
2021-01-27 13:25:33 +0000

    WEBNEW-214 :recycle: Refactored template to use Wrapper

--- src/blog/article/BlogArticlePage.styles.ts
+++ src/blog/article/BlogArticlePage.styles.ts
@@ -1,40 +1,15 @@
 import styled from 'styled-components'
-import {
-  compose,
-  flexbox,
-  layout,
-  space,
-  FlexboxProps,
-  LayoutProps,
-  SpaceProps,
-} from 'styled-system'
 import { RichText } from '../../components/richText'
 import { fontSizes } from '../../theme/fonts'
 import { mediaQueries } from '../../theme/mediaQueries'
 import { spacing } from '../../theme/space'
 
 export const MAX_CONTENT_WIDTH = 624
-export const CONTENT_PADDING_X = {
+
+const CONTENT_PADDING_X = {
   default: 'lg',
   md: 'xxl',
 } as const
-
-export const InfoWrapper = styled.div.attrs({
-  display: 'flex',
-  justifyContent: 'center',
-  marginTop: 'xxl',
-  marginBottom: 'lg',
-})<FlexboxProps & LayoutProps & SpaceProps>`
-  ${compose(flexbox, layout, space)}
-`
-
-export const TitleWrapper = styled.div.attrs({
-  display: 'flex',
-  justifyContent: 'center',
-  paddingX: { default: 'lg', md: 'xxl' },
-})<FlexboxProps & LayoutProps & SpaceProps>`
-  ${compose(flexbox, layout, space)}
-`
 
 export const Content = styled(RichText).attrs({ paddingX: CONTENT_PADDING_X })`
   > p:first-of-type {
@@ -59,14 +34,4 @@ export const Content = styled(RichText).attrs({ paddingX: CONTENT_PADDING_X })`
       max-width: ${MAX_CONTENT_WIDTH}px;
     }
   }
-`
-
-export const AuthorWrapper = styled.div.attrs({
-  display: 'flex',
-  justifyContent: 'center',
-  marginTop: 'xxl',
-  marginBottom: { default: 'xl', md: 'xxl' },
-  paddingX: { default: 'lg', md: 'xxl' },
-})<FlexboxProps & LayoutProps & SpaceProps>`
-  ${compose(flexbox, layout, space)}
 `
--- src/blog/article/BlogArticlePage.tsx
+++ src/blog/article/BlogArticlePage.tsx
@@ -2,19 +2,14 @@ import React from 'react'
 import Image from 'gatsby-image/withIEPolyfill'
 import { RippedSection } from '../../components/rippedSection'
 import { RipVariant } from '../../components/rippedSection/Rip.types'
+import { Wrapper } from '../../components/wrapper'
 import { Tag } from '../../components/tag'
 import { H2 } from '../../components/typography'
 import { TalentProfile } from '../../components/talentProfile'
 import { getImageForBreakpoint } from '../../utils/style-utils'
 import { getRandomInt } from '../../utils/number-utils'
 import { colors } from '../../theme/colors'
-import {
-  MAX_CONTENT_WIDTH,
-  InfoWrapper,
-  TitleWrapper,
-  Content,
-  AuthorWrapper,
-} from './BlogArticlePage.styles'
+import { MAX_CONTENT_WIDTH, Content } from './BlogArticlePage.styles'
 import { BlogArticlePageProps } from './BlogArticlePage.types'
 
 const getCategoryColor = (category: string) => {
@@ -50,19 +45,34 @@ export const BlogArticlePage: React.FC<BlogArticlePageProps> = ({
         aria-hidden="true"
       />
     </RippedSection>
-    <InfoWrapper>
+    <Wrapper
+      display="flex"
+      justifyContent="center"
+      marginTop="xxl"
+      marginBottom="lg"
+    >
       <Tag color={getCategoryColor(category)}>{category}</Tag>
-    </InfoWrapper>
-    <TitleWrapper>
+    </Wrapper>
+    <Wrapper
+      display="flex"
+      justifyContent="center"
+      paddingX={{ default: 'lg', md: 'xxl' }}
+    >
       <H2 as="h1" textAlign="center" maxWidth={842}>
         {title}
       </H2>
-    </TitleWrapper>
+    </Wrapper>
     <Content document={json} />
     {author && (
-      <AuthorWrapper>
+      <Wrapper
+        display="flex"
+        justifyContent="center"
+        marginTop="xxl"
+        marginBottom={{ default: 'xl', md: 'xxl' }}
+        paddingX={{ default: 'lg', md: 'xxl' }}
+      >
         <TalentProfile type="author" {...author} maxWidth={MAX_CONTENT_WIDTH} />
-      </AuthorWrapper>
+      </Wrapper>
     )}
   </>
 )
