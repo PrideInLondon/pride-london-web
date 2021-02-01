a281d5ccbd80fb53c31746e29ec2d72207f0f9cf
Em McDonald <mcdonald.emily@live.co.uk>
2021-02-01 22:25:33 +0000

    WEBNEW-228 :recycle: Refactored story to highlight responsive variant

--- src/components/shareBar/ShareBar.stories.tsx
+++ src/components/shareBar/ShareBar.stories.tsx
@@ -1,24 +1,22 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { text } from '@storybook/addon-knobs'
+import { select, text } from '@storybook/addon-knobs'
 import { ShareBar } from './ShareBar'
+import { VARIANTS } from './ShareBar.types'
 
-const CONTENT = 'Content'
+const CONTENT_GROUP_ID = 'Content'
 
-const dynamicContent = () => ({
-  title: text('Title', 'Pride in London', CONTENT),
-  body: text(
-    'Body',
-    'The UK’s biggest, most diverse Pride. A home for every part of London’s LGBT+ community',
-    CONTENT
-  ),
-  url: text('URL', 'http://prideinlondon.org', CONTENT),
-})
-
-storiesOf('ShareBar', module)
-  .add('horizontal', () => (
-    <ShareBar variant="horizontal" content={dynamicContent()} />
-  ))
-  .add('vertical', () => (
-    <ShareBar variant="vertical" content={dynamicContent()} />
-  ))
+storiesOf('ShareBar', module).add('default', () => (
+  <ShareBar
+    variant={select('variant', VARIANTS, VARIANTS[0])}
+    content={{
+      title: text('Title', 'Pride in London', CONTENT_GROUP_ID),
+      body: text(
+        'Body',
+        'The UK’s biggest, most diverse Pride. A home for every part of London’s LGBT+ community',
+        CONTENT_GROUP_ID
+      ),
+      url: text('URL', 'http://prideinlondon.org', CONTENT_GROUP_ID),
+    }}
+  />
+))
