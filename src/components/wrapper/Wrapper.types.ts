8fbc414f07ebeee16084300cfd9ad4d0100826f0
Em McDonald <mcdonald.emily@live.co.uk>
2021-02-03 23:50:39 +0000

    WEBNEW-230 :label: Moved wrapper props to types file

---
+++ src/components/wrapper/Wrapper.types.ts
@@ -0,0 +1,16 @@
+import {
+  SpaceProps,
+  LayoutProps,
+  TypographyProps,
+  FlexboxProps,
+  PositionProps,
+  ColorProps,
+} from 'styled-system'
+
+export interface WrapperProps
+  extends SpaceProps,
+    LayoutProps,
+    TypographyProps,
+    FlexboxProps,
+    PositionProps,
+    ColorProps {}
