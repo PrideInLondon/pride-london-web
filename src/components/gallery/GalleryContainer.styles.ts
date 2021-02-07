8aeec97c5d0f9420f6cde746cb6e4699277b04cb
Thomas Price <thomas.price@moneysavingexpert.com>
2021-02-07 20:29:36 +0000

    WEBREF-125 :construction: WEBREF-125 Revert Grid changes

--- src/components/gallery/GalleryContainer.styles.ts
+++ src/components/gallery/GalleryContainer.styles.ts
@@ -1,16 +1,27 @@
 import styled from 'styled-components'
-import { media } from '../../theme/media'
+import { layout, LayoutProps, SpaceProps } from 'styled-system'
+import { sm } from '../../theme/space'
 
-export const Grid = styled.div`
-  display: grid;
-  grid-gap: 1.6rem;
-  ${media.mobile`
-    grid-template-columns: repeat(1, 1fr);
-  `}
-  ${media.tablet`
-    grid-template-columns: repeat(2, 1fr);
-  `}
-  ${media.desktop`
-    grid-template-columns: repeat(3, 1fr);
-  `}
+export const Grid = styled.div<LayoutProps & SpaceProps>`
+  display: flex;
+  flex-wrap: wrap;
+  > * {
+    padding: ${sm}px;
+    ${layout}
+  }
 `
+
+// import { media } from '../../theme/media'
+// export const Grid = styled.div`
+//   display: grid;
+//   grid-gap: 1.6rem;
+//   ${media.mobile`
+//     grid-template-columns: repeat(1, 1fr);
+//   `}
+//   ${media.tablet`
+//     grid-template-columns: repeat(2, 1fr);
+//   `}
+//   ${media.desktop`
+//     grid-template-columns: repeat(3, 1fr);
+//   `}
+// `
