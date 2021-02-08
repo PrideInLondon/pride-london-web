2db6effc59afbdb7ef775169dec58dce9db83eb6
Em McDonald <mcdonald.emily@live.co.uk>
2021-02-08 19:29:15 +0000

    WEBNEW-239 :lipstick: Added logic for logo component render

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { colors } from '../../theme/colors'
+import { Mono, Rainbow } from './variants'
 import { LogoProps } from './Logo.types'
 
-export const Logo: React.FC<LogoProps> = ({ variant }) => (
-  <>placeholder for {variant} logo</>
-)
+export const Logo: React.FC<LogoProps> = ({ variant, ...props }) =>
+  variant === 'rainbow' ? (
+    <Rainbow {...props} />
+  ) : (
+    <Mono fill={colors[variant]} {...props} />
+  )
