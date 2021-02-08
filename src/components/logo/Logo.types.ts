9f2375fff3e8efbf4236a5a5b8e73de77f718949
Em McDonald <mcdonald.emily@live.co.uk>
2021-02-08 19:24:56 +0000

    WEBNEW-239 :label: Tweaked logo variant to not be a responsive value

--- src/components/logo/Logo.types.ts
+++ src/components/logo/Logo.types.ts
@@ -1,9 +1,9 @@
-import { ResponsiveValue } from 'styled-system'
+import { SVGProps } from 'react'
 
 export const VARIANTS = ['indigo', 'rainbow', 'white'] as const
 
 type LogoVariant = typeof VARIANTS[number]
 
-export interface LogoProps {
-  variant: ResponsiveValue<LogoVariant>
+export interface LogoProps extends SVGProps<SVGSVGElement> {
+  variant: LogoVariant
 }
