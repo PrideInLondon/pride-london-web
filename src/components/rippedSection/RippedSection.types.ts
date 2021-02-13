02a623152900a0e4ebfcf24d58ddce0e9ee5c638
Em McDonald <mcdonald.emily@live.co.uk>
2021-02-13 20:18:16 +0000

    WEBNEW-216 :recycle: Updated ripped section component to use wrapper

--- src/components/rippedSection/RippedSection.types.ts
+++ src/components/rippedSection/RippedSection.types.ts
@@ -1,7 +1,7 @@
-import { LayoutProps, SpaceProps } from 'styled-system'
+import { WrapperProps } from '../wrapper/Wrapper.types'
 import { Rip } from './Rip.types'
 
-export interface RippedSectionProps extends LayoutProps, SpaceProps {
+export interface RippedSectionProps extends WrapperProps {
   rips?: {
     top?: Rip
     bottom?: Rip
