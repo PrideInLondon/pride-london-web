5fa84746939b1ec8a7a2e97020e18d08672bbdda
Em McDonald <mcdonald.emily@live.co.uk>
2021-02-13 19:44:10 +0000

    WEBNEW-216 :sparkles: Added pagination to article gallery on blog page

--- src/blog/BlogPage.tsx
+++ src/blog/BlogPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Banner } from '../components/banner'
 import { Gallery } from '../components/gallery'
+import { Wrapper } from '../components/wrapper'
+import { Button } from '../components/button'
 import { BlogArticleSummaryCard } from './article/BlogArticleSummaryCard'
 import { StyledGalleryContainer } from './BlogPage.styles'
 import { BlogPageProps } from './BlogPage.types'
@@ -15,18 +17,32 @@ export const BlogPage: React.FC<BlogPageProps> = ({
     <Banner titleText={title} subtitleText={subtitle} fixed={bannerImage} />
     <Gallery
       entries={edges}
-      render={({ entries }) => (
-        <StyledGalleryContainer
-          variant="masonry"
-          columns={{ default: 1, md: 2, lg: 3 }}
-          marginY={{ default: 'xl', md: 'xxl' }}
-          paddingX={{ default: 'lg', md: 'xxl' }}
-          gridGap={32}
-        >
-          {entries.map(({ node }) => (
-            <BlogArticleSummaryCard {...node} />
-          ))}
-        </StyledGalleryContainer>
+      pageSize={9}
+      render={({ entries: pagedEntries, showNextPage }) => (
+        <>
+          <StyledGalleryContainer
+            variant="masonry"
+            columns={{ default: 1, md: 2, lg: 3 }}
+            marginY={{ default: 'xl', md: 'xxl' }}
+            paddingX={{ default: 'lg', md: 'xxl' }}
+            gridGap={32}
+          >
+            {pagedEntries.map(({ node }) => (
+              <BlogArticleSummaryCard {...node} />
+            ))}
+          </StyledGalleryContainer>
+          {pagedEntries.length < edges.length && (
+            <Wrapper
+              backgroundColor="indigo"
+              textAlign="center"
+              paddingY={{ default: 'xl', md: 'xxl' }}
+            >
+              <Button variant="outline-white" onClick={showNextPage}>
+                Show more articles
+              </Button>
+            </Wrapper>
+          )}
+        </>
       )}
     />
   </>
