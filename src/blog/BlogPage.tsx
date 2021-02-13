9fb7df45d167cb1e82399a7a72c43a99c91328dc
Em McDonald <mcdonald.emily@live.co.uk>
2021-02-13 19:20:12 +0000

    WEBNEW-216 :sparkles: Added blog article gallery to blog page

--- src/blog/BlogPage.tsx
+++ src/blog/BlogPage.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import { Banner } from '../components/banner'
+import { Gallery } from '../components/gallery'
+import { BlogArticleSummaryCard } from './article/BlogArticleSummaryCard'
+import { StyledGalleryContainer } from './BlogPage.styles'
 import { BlogPageProps } from './BlogPage.types'
 
 export const BlogPage: React.FC<BlogPageProps> = ({
   data: {
     content: { title, subtitle, bannerImage },
+    blogArticles: { edges },
   },
-}) => <Banner titleText={title} subtitleText={subtitle} fixed={bannerImage} />
+}) => (
+  <>
+    <Banner titleText={title} subtitleText={subtitle} fixed={bannerImage} />
+    <Gallery
+      entries={edges}
+      render={({ entries }) => (
+        <StyledGalleryContainer
+          variant="masonry"
+          columns={{ default: 1, md: 2, lg: 3 }}
+          marginY={{ default: 'xl', md: 'xxl' }}
+          paddingX={{ default: 'lg', md: 'xxl' }}
+          gridGap={32}
+        >
+          {entries.map(({ node }) => (
+            <BlogArticleSummaryCard {...node} />
+          ))}
+        </StyledGalleryContainer>
+      )}
+    />
+  </>
+)
