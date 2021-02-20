ef6e2ddaef8fc4228c1a0eddb84924cf40143cff
Em McDonald <mcdonald.emily@live.co.uk>
2021-02-20 20:05:34 +0000

    WEBNEW-162 :lipstick: Updated color prop to not be used for rip
    This will instead be used for the background component

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { colors } from '../../theme/colors'
 import { RippedSection } from '../rippedSection'
 import { Wrapper, HeroCopyWrapper } from './Hero.styles'
 import { HeroProps } from './Hero.types'
@@ -8,12 +7,12 @@ const HeroCopy: React.FC = ({ children }) => {
   return <HeroCopyWrapper>{children}</HeroCopyWrapper>
 }
 
-export const Hero: React.FC<HeroProps> = ({ title, image, color }) => (
+export const Hero: React.FC<HeroProps> = ({ title, image }) => (
   <RippedSection
     rips={{
       bottom: {
         variant: 1, // until PR #1911 is merged in
-        color: color ? color : colors.white,
+        color: 'white',
       },
     }}
   >
