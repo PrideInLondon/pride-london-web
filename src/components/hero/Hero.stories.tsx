4548615a42dce63f7dbb8408853da286bc2d3990
Em McDonald <mcdonald.emily@live.co.uk>
2021-02-20 12:41:34 +0000

    WEBNEW-162 :fire: Removed old hero asset

--- src/components/hero/Hero.stories.tsx
+++ src/components/hero/Hero.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Hero } from './Hero'
-import PinkHero from './backgrounds/hero-img-pink.jpg'
+import PinkHero from './assets/pink.jpg'
 
 storiesOf('Hero', module).add('default', () => (
   <Hero background={PinkHero} text="Three line heading will go here" />
