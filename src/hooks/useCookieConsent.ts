1ac1e35893982e624ce2a50c21813e49782bbd6b
Jason Lai <jase_lai@hotmail.com>
2021-02-24 21:21:28 +0000

    rename env var

--- src/hooks/useCookieConsent.ts
+++ src/hooks/useCookieConsent.ts
@@ -4,14 +4,17 @@ export const useCookieConsent = () => {
   useEffect(() => {
     if (
       process.env.NODE_ENV === 'production' &&
-      process.env.GATSBY_USERCENTRICS
+      process.env.GATSBY_USERCENTRICS_ID
     ) {
       const script = document.createElement('script')
 
       script.src = 'https://app.usercentrics.eu/browser-ui/latest/bundle.js'
       script.defer = true
       script.id = 'usercentrics-cmp'
-      script.setAttribute('data-settings-id', process.env.GATSBY_USERCENTRICS)
+      script.setAttribute(
+        'data-settings-id',
+        process.env.GATSBY_USERCENTRICS_ID
+      )
       document.body.appendChild(script)
 
       return () => {
