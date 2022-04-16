a0a795f5722e570807dc761bf9d466212579c87d
Shrouded Shrew <shroudedshrew@gmail.com>
2022-04-16 12:12:43 +0100

    WEBNEW-270 :fire: Remove you me us we

--- src/campaigns/youMeUsWe/share.tsx
+++
@@ -1,9 +0,0 @@
-import { ShareContent } from '../../utils/share-utils'
-
-const URL = 'https://prideinlondon.org/ymuw'
-
-export const content: ShareContent = {
-  title: 'You! Me! Us! We!',
-  body: `Celebrating allyship and standing up for one another #YouMeUsWe is a rallying cry for Pride 2020 ${URL} #PrideInLondon`,
-  url: URL,
-}
