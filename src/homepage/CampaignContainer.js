f3e9b5837ae1ab4eec7ffdd7a14ecf1637c2dac7
Shrouded Shrew <shroudedshrew@gmail.com>
2022-05-08 22:45:08 +0100

    WEBNEW-272 :sparkles: Change campaign to Proud and Loud on home page

--- src/homepage/CampaignContainer.js
+++ src/homepage/CampaignContainer.js
@@ -2,32 +2,19 @@ import React from 'react'
 import {
   Container,
   BackgroundImage,
-  TextContainer,
-  StyledHeader,
-  // StyledDescription,
-  // StyledButton,
+  StyledButton,
 } from './CampaignContainer.styles'
 
 const ParadeApplications = () => (
   <Container>
     <BackgroundImage />
-    <TextContainer>
-      <StyledHeader>
-        Pride in London calls for Visibility, Unity and Equality
-      </StyledHeader>
-      {/* To be replaced once arrangements for pride 2022 are confirmed */}
-      {/* <StyledDescription>
-        This Pride month we're calling on all LGBT+ people to make an act of
-        allyship.
-      </StyledDescription> */}
-      {/* <StyledButton
-        to="https://prideinlondon.org/about-us"
-        target="_self"
-        id="campaign-you-me-us-we"
-      >
-        Find out more
-      </StyledButton> */}
-    </TextContainer>
+    <StyledButton
+      to="https://www.royalalberthall.com/tickets/events/2022/proud-and-loud/"
+      target="_self"
+      id="campaign-proud-and-loud"
+    >
+      Buy tickets
+    </StyledButton>
   </Container>
 )
 
