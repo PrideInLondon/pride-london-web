58f0e268bcc433844d785db2f90d07f66bae4170
Shrouded Shrew <{ID}+{username}@users.noreply.github.com>
2022-06-06 19:30:35 +0100

    WEBNEW-274 :sparkles: Add All our pride banner

--- src/homepage/CampaignContainer.js
+++ src/homepage/CampaignContainer.js
@@ -1,31 +1,9 @@
 import React from 'react'
-import {
-  Container,
-  BackgroundImage,
-  StyledButton,
-  TextContainer,
-  StyledHeader,
-  BackgroundImageButton,
-} from './CampaignContainer.styles'
-
-const buyTicketsProps = {
-  to: 'https://www.royalalberthall.com/tickets/events/2022/proud-and-loud/',
-  target: '_self',
-  id: 'campaign-proud-and-loud',
-  children: 'Buy tickets',
-}
+import { Container, BackgroundImage } from './CampaignContainer.styles'
 
 const ParadeApplications = () => (
   <Container>
-    <BackgroundImage>
-      <BackgroundImageButton {...buyTicketsProps} />
-    </BackgroundImage>
-    <TextContainer>
-      <StyledHeader>
-        A special anniversary concert celebrating 50 years of Pride in the UK
-      </StyledHeader>
-    </TextContainer>
-    <StyledButton {...buyTicketsProps} />
+    <BackgroundImage />
   </Container>
 )
 
