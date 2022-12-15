8bd4abda6fcee40e08754fbd5c1a38dd2f888a3d
Zed Spencer-Milnes <zedsm@prideinlondon.org>
2022-12-15 13:12:09 +0000

    :hammer: Remove requirement for JIRA board ticket

--- src/scripts/prepare-commit-msg/git-utils.js
+++ src/scripts/prepare-commit-msg/git-utils.js
@@ -1,33 +1,19 @@
 const path = require('path')
-const { execSync } = require('child_process')
 const fs = require('fs')
 
 const GIT_DIR = path.join(__dirname, '../../../', '.git')
 
 const GIT_MESSAGE_PATH = path.join(GIT_DIR, 'COMMIT_EDITMSG')
 
-const getBranchName = () =>
-  execSync('git rev-parse --abbrev-ref HEAD').toString()
-
-const getJiraInfoFromBranch = () => {
-  const branchName = getBranchName()
-  const result = /^(?<boardId>[A-Z]+)-(?<ticketId>\d+)/.exec(branchName)
-  return (result && result.groups) || {}
-}
-
 const readGitCommitMessage = () =>
   fs.readFileSync(GIT_MESSAGE_PATH, 'utf8').trim()
 
-const writeGitCommitMessage = ({ boardId, ticketId, gitmoji, message }) =>
-  fs.writeFileSync(
-    GIT_MESSAGE_PATH,
-    `${boardId}-${ticketId} ${gitmoji} ${message}`
-  )
+const writeGitCommitMessage = ({ gitmoji, message }) =>
+  fs.writeFileSync(GIT_MESSAGE_PATH, `${gitmoji} ${message}`)
 
 const isRebasing = () => fs.existsSync(path.join(GIT_DIR, 'rebase-merge'))
 
 module.exports = {
-  getJiraInfoFromBranch,
   readGitCommitMessage,
   writeGitCommitMessage,
   isRebasing,
--- src/scripts/prepare-commit-msg/prepare-commit-msg.js
+++ src/scripts/prepare-commit-msg/prepare-commit-msg.js
@@ -1,9 +1,7 @@
 const inquirer = require('inquirer')
-const chalk = require('chalk')
 const fuzzy = require('fuzzy')
 const getGitmojis = require('./gitmoji-utils')
 const {
-  getJiraInfoFromBranch,
   readGitCommitMessage,
   writeGitCommitMessage,
   isRebasing,
@@ -13,22 +11,6 @@ inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'))
 
 const prepareCommitMessage = () => {
   if (isRebasing()) return
-
-  const jiraInfo = getJiraInfoFromBranch()
-  if (!jiraInfo.boardId || !jiraInfo.ticketId) {
-    console.error(
-      chalk.red('\nYou must be on a branch with name ') +
-        chalk.yellow('JIRABOARD-JIRATICKET/description-of-ticket') +
-        chalk.red(' to continue (e.g. ') +
-        chalk.yellow('WEBREF-123/migrating-component-x-to-hooks') +
-        chalk.red(')\n')
-    )
-    process.exit(1)
-  }
-
-  console.log(
-    chalk.cyan(`\nTagging commit as ${jiraInfo.boardId}-${jiraInfo.ticketId}\n`)
-  )
 
   getGitmojis().then((gitmojis) => {
     inquirer
@@ -65,7 +47,6 @@ const prepareCommitMessage = () => {
       .then((answers) =>
         writeGitCommitMessage({
           message: readGitCommitMessage(),
-          ...jiraInfo,
           ...answers,
         })
       )
