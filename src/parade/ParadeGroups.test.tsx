666d2c0aefa2e07579085247f76ed5aadb250d1e
howardlhw-pil <wuen2001@gmail.com>
2023-06-24 05:26:24 +0100

    linting #4

--- src/parade/ParadeGroups.test.tsx
+++ src/parade/ParadeGroups.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
+// import { fireEvent } from '@testing-library/react'
 import { axe } from 'jest-axe'
 import theme from '../theme/theme'
 import ParadeGroups from './ParadeGroups'
